ddeac73dbaf0729db0da6ffa085206ce4d1a3e92
Sui Yang <suiyang03@gmail.com>
2021-03-21 14:22:07 +0800

    Time: 88 ms (86.68%), Space: 18 MB (10.48%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.py
+++ deepest-leaves-sum/deepest-leaves-sum.py
@@ -9,17 +9,39 @@ class Solution:
         # Time: O(N)
         # Space: O(height)
         
-        # Iterative
-        d = defaultdict(int)
-        l = 0
-        stack = [(root, 0)]
-        while stack:
-            node, level = stack.pop()
-            if node:
-                if not node.left and not node.right:
-                    d[level] += node.val
-                    l = max(l, level)
-                stack.append((node.left, level+1))
-                stack.append((node.right, level+1))
-            
-        return d[l]
+        # # Iterative 1
+        # d = defaultdict(int)
+        # l = 0
+        # stack = [(root, 0)]
+        # while stack:
+        #     node, level = stack.pop()
+        #     if node:
+        #         if not node.left and not node.right:
+        #             d[level] += node.val
+        #             l = max(l, level)
+        #         stack.append((node.left, level+1))
+        #         stack.append((node.right, level+1))
+        # return d[l]
+        
+        # Iterative 2
+        next_level = [root]
+        while next_level:
+            cur_level = next_level
+            next_level = []
+            for node in cur_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        return sum([node.val for node in cur_level])
+        
+        # # Recursive
+        # d = defaultdict(int)
+        # def preorder(node, level=0):
+        #     if not node: return
+        #     d[level] += node.val
+        #     preorder(node.left, level+1)
+        #     preorder(node.right, level+1)
+        # preorder(root)
+        # _, ans = d.popitem()
+        # return ans

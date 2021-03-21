9325909e1f3ae8210e9a4b0ae27f05985ed666b7
Sui Yang <suiyang03@gmail.com>
2021-03-21 12:04:24 +0800

    Time: 32 ms (63.11%), Space: 14.2 MB (50.44%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -12,16 +12,26 @@ class Solution:
         # # Recursive
         # return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val] if root else []
         
-        # Iterative (Cool Version)
-        # Using flag to record if the node is visited
-        res, stack = [], [(root, False)]
+        # # Iterative (Cool Version)
+        # # Using flag to record if the node is visited
+        # res, stack = [], [(root, False)]
+        # while stack:
+        #     node, visited = stack.pop()
+        #     if node:
+        #         if visited:
+        #             res.append(node.val)
+        #         else:
+        #             stack.append((node, True))
+        #             stack.append((node.right, False))
+        #             stack.append((node.left, False))
+        # return res
+        
+        # Iterative 2
+        res, stack = [], [root]
         while stack:
-            node, visited = stack.pop()
+            node = stack.pop()
             if node:
-                if visited:
-                    res.append(node.val)
-                else:
-                    stack.append((node, True))
-                    stack.append((node.right, False))
-                    stack.append((node.left, False))
-        return res
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]

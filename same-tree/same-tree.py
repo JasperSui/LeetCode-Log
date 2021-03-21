da75225470abaa6d62c6bc9885e48a95ebf5b91b
Sui Yang <suiyang03@gmail.com>
2021-03-21 14:59:35 +0800

    Time: 28 ms (84.67%), Space: 14 MB (96.91%) - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -9,19 +9,23 @@ class Solution:
         # Time: O(N)
         # Space: O(height)
         
-        # Iterative
-        sp = [p]
-        sq = [q]
-        while sp or sq:
-            n1 = sp.pop()
-            n2 = sq.pop()
-            if n1 or n2:
-                if not n1 or not n2: return False
-                if n1.val != n2.val: return False
-                sp.append(n1.left)
-                sp.append(n1.right)
-                sq.append(n2.left)
-                sq.append(n2.right)
-        return True
-            
-            
+        # # Iterative
+        # sp = [p]
+        # sq = [q]
+        # while sp or sq:
+        #     n1 = sp.pop()
+        #     n2 = sq.pop()
+        #     if n1 or n2:
+        #         if not n1 or not n2: return False
+        #         if n1.val != n2.val: return False
+        #         sp.append(n1.left)
+        #         sp.append(n1.right)
+        #         sq.append(n2.left)
+        #         sq.append(n2.right)
+        # return True
+        
+        # Recursive
+        if not p or not q: return p == q
+        if p.val != q.val: return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)

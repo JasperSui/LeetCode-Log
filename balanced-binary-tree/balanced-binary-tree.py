a2752d8d8426fc8672028d2f2514dda1556c5cd9
Sui Yang <suiyang03@gmail.com>
2021-03-23 23:50:58 +0800

    Time: 52 ms (61.07%), Space: 19 MB (43.80%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.py
+++ balanced-binary-tree/balanced-binary-tree.py
@@ -7,15 +7,29 @@
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         
-        # Solution 1
-        # Time: O(nlogn)
+        # # Solution 1
+        # # Time: O(nlogn)
+        # # Space: O(height)
+        # if not root: return True
+        # def get_height(node):
+        #     if not node: return 0
+        #     return max(get_height(node.left), get_height(node.right)) +1
+        # left_height = get_height(root.left)
+        # right_height = get_height(root.right)
+        # return (abs(left_height - right_height) <= 1) and \
+        #        self.isBalanced(root.left) and \
+        #        self.isBalanced(root.right)
+        
+        # Solution 2
+        # Time: O(n)
         # Space: O(height)
+        
         if not root: return True
-        def get_height(node):
+        self.is_balanced = True
+        def dfs(node):
             if not node: return 0
-            return max(get_height(node.left), get_height(node.right)) +1
-        left_height = get_height(root.left)
-        right_height = get_height(root.right)
-        return (abs(left_height - right_height) <= 1) and \
-               self.isBalanced(root.left) and \
-               self.isBalanced(root.right)
+            left, right = dfs(node.left), dfs(node.right)
+            if abs(left - right) > 1: self.is_balanced = False
+            return max(left, right) + 1
+        dfs(root)
+        return self.is_balanced

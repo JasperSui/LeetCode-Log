f531a94a067b825f475380b083979ca31771ab53
Sui Yang <suiyang03@gmail.com>
2021-03-24 21:31:07 +0800

    Time: 28 ms (95.70%), Space: 14.7 MB (48.76%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -9,11 +9,36 @@ class Solution:
         # Time: O(n)
         # Space: O(height)
 
-        d = defaultdict(list)
-        def dfs(node, level=0):
-            if node:
-                d[level].append(node.val)
-                dfs(node.left, level+1)
-                dfs(node.right, level+1)
-        dfs(root)
-        return list(d.values())
+        # # Recursive
+        # d = defaultdict(list)
+        # def dfs(node, level=0):
+        #     if node:
+        #         d[level].append(node.val)
+        #         dfs(node.left, level+1)
+        #         dfs(node.right, level+1)
+        # dfs(root)
+        # return list(d.values())
+    
+        # Iterative
+        queue = deque([root])
+        next_level = []
+        res = []
+        while queue or next_level:
+            curr_level = []
+            while queue:
+                curr = queue.popleft()
+                curr_level.append(curr)
+            next_level = curr_level
+            if next_level:
+                temp_list = []
+                for n in next_level:
+                    if n:
+                        temp_list.append(n.val)
+                        if n.left: queue.append(n.left)
+                        if n.right: queue.append(n.right)
+                if temp_list: res.append(temp_list)
+                next_level = []
+        return res
+                
+                
+                

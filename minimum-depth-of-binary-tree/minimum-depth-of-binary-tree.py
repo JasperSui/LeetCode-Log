0976eb0eb795275dfae2a3b097575216a78c1d42
Sui Yang <suiyang03@gmail.com>
2021-03-24 09:11:22 +0800

    Time: 480 ms (82.32%), Space: 49.4 MB (60.93%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -9,13 +9,24 @@ class Solution:
         # Time: O(n)
         # Space: O(height)
         
-        # Solution 1 (Recursive)
+        # # Solution 1 (Recursive) 828ms
+        # if not root: return 0
+        # d = list(map(self.minDepth, (root.left, root.right)))
+        # return 1 + (min(d) or max(d))
+        
+        # Solution 2 (Iterative, BFS)
         if not root: return 0
-        d = list(map(self.minDepth, (root.left, root.right)))
-        return 1 + (min(d) or max(d))
+        queue = deque([(root, 1)])
+        level = 0
+        while queue:
+            curr, level = queue.popleft()
+            if curr:
+                if not curr.left and not curr.right:
+                    return level
+                queue.append((curr.left, level+1))
+                queue.append((curr.right, level+1))
         
-        
-        # # Solution 3 (Stupid)
+        # # Solution 3 (Stupid, DFS) 612ms
         # self.min_depth = 50000000
         # if not root: return 0
         # def dfs(node, depth):

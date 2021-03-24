c13f8c0520a7bfc40f13a9cea22f627cdac49655
Sui Yang <suiyang03@gmail.com>
2021-03-25 00:57:03 +0800

    Time: 36 ms (62.80%), Space: 14.7 MB (50.62%) - LeetHub

--- binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -10,12 +10,36 @@ class Solution:
         # Space: O(height)
         
         # Recursive
+        # if not root: return []
+        # d = defaultdict(list)
+        # def dfs(node, level):
+        #     if not node: return
+        #     d[level].append(node.val)
+        #     dfs(node.left, level+1)
+        #     dfs(node.right, level+1)
+        # dfs(root, 0)
+        # return list(d.values())[::-1]
+    
+        # Iterative
         if not root: return []
-        d = defaultdict(list)
-        def dfs(node, level):
-            if not node: return
-            d[level].append(node.val)
-            dfs(node.left, level+1)
-            dfs(node.right, level+1)
-        dfs(root, 0)
-        return list(d.values())[::-1]
+        queue = deque([root])
+        res = deque()
+        next_level = []
+        while queue or next_level:
+            curr_level = []
+            while queue:
+                curr = queue.popleft()
+                if curr:
+                    curr_level.append(curr)
+            next_level = curr_level
+            if next_level:
+                temp_list = []
+                for n in next_level:
+                    if n:
+                        temp_list.append(n.val)
+                        queue.append(n.left)
+                        queue.append(n.right)
+                if temp_list:
+                    res.appendleft(temp_list)
+        return list(res)
+        

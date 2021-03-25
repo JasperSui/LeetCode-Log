0b0505c3a89e54f5a5b4386c37ca0c2d7fe776d0
Sui Yang <suiyang03@gmail.com>
2021-03-25 22:42:36 +0800

    Time: 36 ms (34.43%), Space: 14.3 MB (69.82%) - LeetHub

--- leaf-similar-trees/leaf-similar-trees.py
+++ leaf-similar-trees/leaf-similar-trees.py
@@ -9,12 +9,32 @@ class Solution:
         # Time: O(N)
         # Space: O(height)
         
-        # Recursive
-        def dfs(n, l):
-            if n.left: dfs(n.left, l)
-            if n.right: dfs(n.right, l)
-            if not n.left and not n.right:
-                l.append(n.val)
-            return l # Should return l at every single last situation
-
-        return dfs(root1, []) == dfs(root2, [])
+        # # Recursive
+        # def dfs(n, l):
+        #     if n.left: dfs(n.left, l)
+        #     if n.right: dfs(n.right, l)
+        #     if not n.left and not n.right:
+        #         l.append(n.val)
+        #     return l # Should return l at every single last situation
+        # return dfs(root1, []) == dfs(root2, [])
+        
+        # Iterative
+        s1, s2, l1, l2 = [root1], [root2], [], []
+        while s1:
+            curr = s1.pop()
+            if curr:
+                if not curr.left and not curr.right: 
+                    l1.append(curr.val)
+                    continue
+                s1.append(curr.left)
+                s1.append(curr.right)
+                
+        while s2:
+            curr = s2.pop()
+            if curr:
+                if not curr.left and not curr.right: 
+                    l2.append(curr.val)
+                    continue
+                s2.append(curr.left)
+                s2.append(curr.right)
+        return l1 == l2

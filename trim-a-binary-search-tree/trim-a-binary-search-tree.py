dffad956a6a91ba9c03a5335aadeed10ed1f1be8
Sui Yang <suiyang03@gmail.com>
2021-03-27 11:52:58 +0800

    Time: 52 ms (73.58%), Space: 18.1 MB (79.38%) - LeetHub

--- trim-a-binary-search-tree/trim-a-binary-search-tree.py
+++ trim-a-binary-search-tree/trim-a-binary-search-tree.py
@@ -9,12 +9,33 @@ class Solution:
         # Time: O(n)
         # Space: O(height)
         
-        # DFS
-        if not root: return None
-        if root.val > R: return self.trimBST(root.left, L, R)
-        if root.val < L: return self.trimBST(root.right, L, R)
+        # # DFS
+        # if not root: return None
+        # if root.val > R: return self.trimBST(root.left, L, R)
+        # if root.val < L: return self.trimBST(root.right, L, R)
+
+        # root.left = self.trimBST(root.left, L, R)
+        # root.right = self.trimBST(root.right, L, R)
         
-        root.left = self.trimBST(root.left, L, R)
-        root.right = self.trimBST(root.right, L, R)
+        # return root
         
+        # Iterative
+        while root and not (L <= root.val <= R):
+            if root.val < L: root = root.right
+            elif root.val >R: root = root.left
+        stack = [root]
+        while stack:
+            curr = stack[-1]
+            if curr is None: stack.pop(); continue
+            updated = 0
+            if curr.left and curr.left.val < L:
+                curr.left = curr.left.right
+                updated += 1
+            if curr.right and curr.right.val > R:
+                curr.right = curr.right.left
+                updated += 1
+            if not updated:
+                stack.pop()
+                stack.append(curr.left)
+                stack.append(curr.right)
         return root

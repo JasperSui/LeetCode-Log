3e3061ecc8fe2fdb9a011e8cdfea6fea63ead787
Sui Yang <suiyang03@gmail.com>
2021-03-27 13:48:28 +0800

    Time: 40 ms (82.96%), Space: 15.6 MB (99.11%) - LeetHub

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -9,17 +9,27 @@ class Solution:
         # Time: O(n)
         # Space: O(height)
         
-        # DFS
-        ans = False
-        def dfs(node, current_sum):
-            nonlocal ans
-            if not node: return
-            current_sum += node.val
-            if current_sum == targetSum and not node.left and not node.right:
-                ans = True
-            if node.left: dfs(node.left, current_sum)
-            if node.right: dfs(node.right, current_sum)
-        dfs(root, 0)
-        return ans
+        # # DFS
+        # ans = False
+        # def dfs(node, current_sum):
+        #     nonlocal ans
+        #     if not node: return
+        #     current_sum += node.val
+        #     if current_sum == targetSum and not node.left and not node.right:
+        #         ans = True
+        #     if node.left: dfs(node.left, current_sum)
+        #     if node.right: dfs(node.right, current_sum)
+        # dfs(root, 0)
+        # return ans
         
+        # Iterative
+        stack = [(root, 0)]
+        while stack:
+            curr, s = stack.pop()
+            if curr:
+                s += curr.val
+                if not curr.left and not curr.right and s == targetSum: return True
+                stack.append((curr.left, s))
+                stack.append((curr.right, s))
+        return False
             

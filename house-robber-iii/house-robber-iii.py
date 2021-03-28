8bd2db9445433b7e8f8f5e99c210f54658db5c5a
Sui Yang <suiyang03@gmail.com>
2021-03-28 16:28:35 +0800

    Time: 48 ms (74.22%), Space: 16.1 MB (77.74%) - LeetHub

--- house-robber-iii/house-robber-iii.py
+++ house-robber-iii/house-robber-iii.py
@@ -5,19 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.d = {}
+#     def __init__(self):
+#         self.d = {}
 
+#     def rob(self, root: TreeNode) -> int:
+#         # Time: O(n)
+#         # Space: O(height)
+#         if not root: return 0
+#         if root in self.d: return self.d[root]
+#         ans = 0
+#         if root.left:
+#             ans += self.rob(root.left.left) + self.rob(root.left.right)
+#         if root.right:
+#             ans += self.rob(root.right.left) + self.rob(root.right.right)
+#         ans = max(ans + root.val, self.rob(root.left) + self.rob(root.right))
+#         self.d[root] = ans
+#         return ans
+
+    # Genius way
     def rob(self, root: TreeNode) -> int:
-        # Time: O(n)
-        # Space: O(height)
-        if not root: return 0
-        if root in self.d: return self.d[root]
-        ans = 0
-        if root.left:
-            ans += self.rob(root.left.left) + self.rob(root.left.right)
-        if root.right:
-            ans += self.rob(root.right.left) + self.rob(root.right.right)
-        ans = max(ans + root.val, self.rob(root.left) + self.rob(root.right))
-        self.d[root] = ans
-        return ans
+        return max(self.rob_sub(root))
+
+    def rob_sub(self, root):
+        if not root: return (0, 0)
+        left_1, left_2 = self.rob_sub(root.left)
+        right_1, right_2 = self.rob_sub(root.right)
+        res = (max(left_1, left_2) + max(right_1, right_2), root.val + left_1 + right_1)
+        return res

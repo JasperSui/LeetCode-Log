77c123ea822996e95efd08ac54fa0102218d24e8
Sui Yang <suiyang03@gmail.com>
2021-04-03 13:23:59 +0800

    Time: 1108 ms (31.46%), Space: 25.2 MB (53.26%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price, min_index = float('inf'), None
-        max_price, max_index = float('-inf'), None
-        max_profit = 0
+        
         if not prices: return 0
-        for i, p in enumerate(prices):
-            if p <= min_price:
-                min_price = p
-                min_index = i
-            elif p >= max_price or p >= min_price:
-                max_price = p
-                max_index = i
-            if min_index is not None and max_index is not None and min_index < max_index:
-                print(min_index, max_index)
-                max_profit = max(max_profit, max_price-min_price)
+        min_price, max_profit = prices[0], 0
+        for i in range(1, len(prices)):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
         return max_profit
+        
+        # Suck idea
+        # min_price, min_index = float('inf'), None
+        # max_price, max_index = float('-inf'), None
+        # max_profit = 0
+        # if not prices: return 0
+        # for i, p in enumerate(prices):
+        #     if p <= min_price:
+        #         min_price = p
+        #         min_index = i
+        #     elif p >= max_price or p >= min_price:
+        #         max_price = p
+        #         max_index = i
+        #     if min_index is not None and max_index is not None and min_index < max_index:
+        #         print(min_index, max_index)
+        #         max_profit = max(max_profit, max_price-min_price)
+        # return max_profit
         
         # if not prices:
         #     return 0

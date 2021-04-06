af025243d0d64765ca5c55429f2b1fa9d8f5ddd7
Sui Yang <suiyang03@gmail.com>
2021-04-06 11:42:09 +0800

    Time: 404 ms (87.36%), Space: 15.6 MB (21.00%) - LeetHub

---
+++ koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # Binary Search
+        low, high = 1, max(piles)
+        while low < high:
+            mid = (low+high) // 2
+            if sum((p+mid-1)//mid for p in piles) > h:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+        
+        # Shitty try
+        # lp = len(piles)
+        # piles.sort()
+        # if lp <= h < lp*2: return piles[lp-h-1]
+        # else:
+        #     ans = piles[0]
+        #     temp_h = 0
+        #     while temp_h != h:
+        #         for p in piles:
+                    

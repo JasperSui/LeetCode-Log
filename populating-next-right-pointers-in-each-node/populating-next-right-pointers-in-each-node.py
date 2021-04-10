ed59a2ef6c50edb916ce40c01639a64dcf9d7a89
Sui Yang <suiyang03@gmail.com>
2021-04-10 16:09:51 +0800

    Time: 244 ms (5.22%), Space: 15.7 MB (69.69%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,17 +10,33 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root: return root
-        next_level = deque([root])
-        while next_level:
-            curr_level = deque()
-            n = None
-            while next_level:
-                curr = next_level.popleft()
-                if curr.right: curr_level.append(curr.right)
-                if curr.left: curr_level.append(curr.left)
-                curr.next = n
-                n = curr
-            next_level = curr_level
+        # Time: O(n)
+        # Space: O(n)
+        
+        # Iterative
+        # if not root: return root
+        # next_level = deque([root])
+        # while next_level:
+        #     curr_level = deque()
+        #     n = None
+        #     while next_level:
+        #         curr = next_level.popleft()
+        #         if curr.right: curr_level.append(curr.right)
+        #         if curr.left: curr_level.append(curr.left)
+        #         curr.next = n
+        #         n = curr
+        #     next_level = curr_level
+        # return root
+        
+        # Recursive
+        if not root: return None
+        self.connect_left_and_right(root.left, root.right)
         return root
+    
+    def connect_left_and_right(self, n1, n2):
+        if not n1 or not n2: return None
+        n1.next = n2
+        self.connect_left_and_right(n1.left, n1.right)
+        self.connect_left_and_right(n2.left, n2.right)
+        self.connect_left_and_right(n1.right, n2.left)
             

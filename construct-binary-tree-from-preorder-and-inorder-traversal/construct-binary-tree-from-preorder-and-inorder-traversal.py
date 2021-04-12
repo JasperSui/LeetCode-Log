f5d624b4633f7cf812c97b8a0a3d3614daa921d3
Sui Yang <suiyang03@gmail.com>
2021-04-12 09:37:58 +0800

    Time: 148 ms (37.30%), Space: 53.1 MB (45.22%) - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,22 +6,31 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        return self.build(preorder, 0, len(preorder)-1,
-                          inorder, 0, len(inorder)-1)
+        if inorder:
+            index = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index])
+            root.left = self.buildTree(preorder, inorder[0:index])
+            root.right = self.buildTree(preorder, inorder[index+1:])
+            return root
+            
         
-    def build(self, preorder, pre_s, pre_e,
-              inorder, in_s, in_e):
-        if pre_s > pre_e: return
-        root_val = preorder[pre_s]
-        index = 0
-        for i, v in enumerate(inorder):
-            if v == root_val:
-                index = i
-                break
-        left_size = index - in_s
-        root = TreeNode(root_val)
-        root.left = self.build(preorder, pre_s+1, pre_s+left_size,
-                               inorder, in_s, index-1)
-        root.right = self.build(preorder, pre_s+left_size+1, pre_e,
-                               inorder, index+1, in_e)
-        return root
+        
+#         return self.build(preorder, 0, len(preorder)-1,
+#                           inorder, 0, len(inorder)-1)
+        
+#     def build(self, preorder, pre_s, pre_e,
+#               inorder, in_s, in_e):
+#         if pre_s > pre_e: return
+#         root_val = preorder[pre_s]
+#         index = 0
+#         for i, v in enumerate(inorder):
+#             if v == root_val:
+#                 index = i
+#                 break
+#         left_size = index - in_s
+#         root = TreeNode(root_val)
+#         root.left = self.build(preorder, pre_s+1, pre_s+left_size,
+#                                inorder, in_s, index-1)
+#         root.right = self.build(preorder, pre_s+left_size+1, pre_e,
+#                                inorder, index+1, in_e)
+#         return root

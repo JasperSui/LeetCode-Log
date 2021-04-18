c1e2990f085f34813630b69ecd3e5914f76f30de
Sui Yang <suiyang03@gmail.com>
2021-04-18 15:46:09 +0800

    Time: 724 ms (37.99%), Space: 15.5 MB (57.90%) - LeetHub

---
+++ open-the-lock/open-the-lock.py
@@ -0,0 +1,35 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        
+        def plus(s, index):
+            s = list(s)
+            if s[index] == '9': s[index] = '0'
+            else: s[index] = str(int(s[index])+1)
+            return ''.join(s)
+        
+        def minus(s, index):
+            s = list(s)
+            if s[index] == '0': s[index] = '9'
+            else: s[index] = str(int(s[index])-1)
+            return ''.join(s)
+
+        deadends = set(deadends)
+        visited = set('0000')
+        queue = deque(['0000'])
+        steps = 0
+        while queue:
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                if cur in deadends: continue
+                if cur == target: return steps
+                for i in range(4):
+                    up = plus(cur, i)
+                    down = minus(cur, i)
+                    if up not in visited:
+                        queue.append(up)
+                        visited.add(up)
+                    if down not in visited:
+                        queue.append(down)
+                        visited.add(down)
+            steps += 1
+        return -1

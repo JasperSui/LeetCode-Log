dfcea3617165dffb487ef5730daf87816b0adfd6
Sui Yang <suiyang03@gmail.com>
2021-04-24 15:52:24 +0800

    Time: 1060 ms (81.18%), Space: 28.3 MB (64.45%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,19 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # one_buy, two_buy = float('inf'), float('inf')
-        # one_buy_one_sale, two_buy_two_sale = 0, 0
-        # for p in prices:
-        #     one_buy = min(one_buy, p)
-        #     one_buy_one_sale = max(one_buy_one_sale, p - one_buy)
-        #     two_buy = min(two_buy, p - one_buy_one_sale)
-        #     two_buy_two_sale = max(two_buy_two_sale, p - two_buy)
-        # return two_buy_two_sale
-        
-        dp_i_1_0, dp_i_1_1 = 0, float('-inf')
-        dp_i_2_0, dp_i_2_1 = 0, float('-inf')
+        buy1, buy2 = float('-inf'), float('-inf')
+        sell1, sell2 = 0, 0
         for p in prices:
-            dp_i_2_0 = max(dp_i_2_0, dp_i_2_1 + p)
-            dp_i_2_1 = max(dp_i_2_1, dp_i_1_0 - p)
-            dp_i_1_0 = max(dp_i_1_0, dp_i_1_1 + p)
-            dp_i_1_1 = max(dp_i_1_1, -p)
-        return dp_i_2_0
+            buy1 = max(buy1, -p)
+            sell1 = max(sell1, buy1 + p)
+            buy2 = max(buy2, sell1 - p)
+            sell2 = max(sell2, buy2 + p)
+        return sell2
+        
+        # buy = []
+        
+        # dp_i_1_0, dp_i_1_1 = 0, float('-inf')
+        # dp_i_2_0, dp_i_2_1 = 0, float('-inf')
+        # for p in prices:
+        #     dp_i_2_0 = max(dp_i_2_0, dp_i_2_1 + p)
+        #     dp_i_2_1 = max(dp_i_2_1, dp_i_1_0 - p)
+        #     dp_i_1_0 = max(dp_i_1_0, dp_i_1_1 + p)
+        #     dp_i_1_1 = max(dp_i_1_1, -p)
+        # return dp_i_2_0
+        
+        

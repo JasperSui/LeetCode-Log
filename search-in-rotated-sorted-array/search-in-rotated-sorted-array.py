5cf2d37d10df7dd9bb4738e4b88faaf2dda93079
Sui Yang <suiyang03@gmail.com>
2021-05-02 15:01:55 +0800

    Time: 40 ms (69.59%), Space: 14.6 MB (79.23%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,25 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Find smallest number first
-        low, high = 0, len(nums)-1
+        low, high = 0, len(nums) - 1
         while low < high:
-            mid = (low+high) // 2
+            mid = low + (high - low) // 2
             if nums[mid] > nums[high]: low = mid + 1
             else: high = mid
-            print(low, high)
         small_index = low
-
-        # Find out in which side the target lies, means narrow down the nums for what we really want
-        low, high = 0, len(nums)-1
-        if target >= nums[small_index] and target <= nums[high]:
-            low = small_index
-        else: high = small_index
-
-        # Do normal binary search
-        while low <= high:
-            mid = (low+high) // 2
-            if nums[mid] == target: return mid
-            elif nums[mid] > target: high = mid - 1
-            else: low = mid + 1
-        return -1
         
+        low, high = 0, len(nums) - 1
+        if nums[small_index] <= target <= nums[high]:
+            low = small_index
+        else:
+            high = small_index
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target: return mid
+            elif nums[mid] < target: low = mid + 1
+            elif nums[mid] > target: high = mid - 1
+        return -1

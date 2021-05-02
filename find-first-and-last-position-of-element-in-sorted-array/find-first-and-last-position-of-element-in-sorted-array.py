bf092b00ef835e7417a5d3ad0130d4e7248bb5d5
Sui Yang <suiyang03@gmail.com>
2021-05-02 15:16:50 +0800

    Time: 88 ms (35.98%), Space: 15.4 MB (47.96%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,18 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums: return [-1, -1]
+        low, high = 0, len(nums) - 1
+        start, end = 0, 0
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                start = end = mid
+                while start > 0 and nums[start-1] == target:
+                    start -= 1
+                while end < len(nums) - 1 and nums[end+1] == target:
+                    end += 1
+                return [start, end]
+            elif nums[mid] > target: high = mid - 1
+            elif nums[mid] < target: low = mid + 1
+        return [-1, -1]
         
-        # O(n) Version
-        # m, res = 0, []
-        # for i, n in enumerate(nums):
-        #     if n == target: 
-        #         if not res: res.append(i)
-        #         m = max(m, i)
-        # if m: res.append(m)
-        # if len(res) == 1: res += res
-        # return res if res else [-1, -1]
-        
-        # O(logn) Version
-        def get_start_index():
-            low, high, index = 0, len(nums)-1, -1
-            while low <= high:
-                mid = (low+high) // 2
-                if nums[mid] == target:
-                    index = mid
-                    high = mid - 1
-                elif nums[mid] > target: high = mid - 1
-                else: low = mid + 1
-            return index
-        
-        def get_end_index():
-            low, high, index = 0, len(nums)-1, -1
-            while low <= high:
-                mid = (low+high) // 2
-                if nums[mid] == target:
-                    index = mid
-                    low = mid + 1
-                elif nums[mid] > target: high = mid - 1
-                else: low = mid + 1
-            return index
-        
-        left, right = get_start_index(), get_end_index()
-        if left != -1:
-            if right != -1:
-                return [left, right]
-        return [left, left]

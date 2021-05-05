3fd63696fea7a97a76d0cf0dc122a2c3a85be56b
Sui Yang <suiyang03@gmail.com>
2021-05-05 13:20:35 +0800

    Time: 32 ms (52.54%), Space: 14.1 MB (86.14%) - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -6,26 +6,6 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        # Time: O(N)
-        # Space: O(height)
-        
-        # # Iterative
-        # sp = [p]
-        # sq = [q]
-        # while sp or sq:
-        #     n1 = sp.pop()
-        #     n2 = sq.pop()
-        #     if n1 or n2:
-        #         if not n1 or not n2: return False
-        #         if n1.val != n2.val: return False
-        #         sp.append(n1.left)
-        #         sp.append(n1.right)
-        #         sq.append(n2.left)
-        #         sq.append(n2.right)
-        # return True
-        
-        # Recursive
-        if not p or not q: return p == q
+        if not q or not p: return p == q
         if p.val != q.val: return False
-        
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)

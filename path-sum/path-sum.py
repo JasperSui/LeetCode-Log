b0b0cd99c514b2e439aaf8aeff8da25853c57f46
Sui Yang <suiyang03@gmail.com>
2021-05-11 13:14:38 +0800

    Time: 48 ms (17.85%), Space: 16 MB (24.47%) - LeetHub

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -6,30 +6,13 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        # Time: O(n)
-        # Space: O(height)
-        
-        # # DFS
-        # ans = False
-        # def dfs(node, current_sum):
-        #     nonlocal ans
-        #     if not node: return
-        #     current_sum += node.val
-        #     if current_sum == targetSum and not node.left and not node.right:
-        #         ans = True
-        #     if node.left: dfs(node.left, current_sum)
-        #     if node.right: dfs(node.right, current_sum)
-        # dfs(root, 0)
-        # return ans
-        
-        # Iterative
-        stack = [(root, 0)]
-        while stack:
-            curr, s = stack.pop()
-            if curr:
-                s += curr.val
-                if not curr.left and not curr.right and s == targetSum: return True
-                stack.append((curr.left, s))
-                stack.append((curr.right, s))
-        return False
-            
+        self.ans = False
+        def dfs(node, curr_sum=0):
+            if not node: return
+            curr_sum += node.val
+            if curr_sum == targetSum and not node.left and not node.right: self.ans = True
+            dfs(node.left, curr_sum)
+            dfs(node.right, curr_sum)
+            return
+        dfs(root)
+        return self.ans

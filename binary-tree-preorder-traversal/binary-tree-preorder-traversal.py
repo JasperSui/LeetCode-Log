2e68dd94238abfe5ee2cf51541474aa471077c13
Sui Yang <suiyang03@gmail.com>
2021-05-20 12:12:38 +0800

    Time: 20 ms (98.93%), Space: 14.4 MB (11.19%) - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -8,19 +8,27 @@ class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         # Time: O(N)
         # Space: O(height)
+        self.res = []
+        def dfs(node):
+            if not node: return
+            self.res.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return self.res
         
         # # Recursive 1
         # return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
         
         # Recursive 2
-        res = []
-        def dfs(node):
-            if node:
-                res.append(node.val)
-                dfs(node.left)
-                dfs(node.right)
-        dfs(root)
-        return res
+        # res = []
+        # def dfs(node):
+        #     if node:
+        #         res.append(node.val)
+        #         dfs(node.left)
+        #         dfs(node.right)
+        # dfs(root)
+        # return res
     
         # # Iterative
         # if not root: return []
@@ -32,4 +40,16 @@ class Solution:
         #     if curr.left: stack.append(curr.left)
             
         # return res
+        
+        
+        
+        def preorder(node):
+            if node is None: return None
+            print(node.val)
+            inorder(node.left)
+            inorder(node.right)
+
+        preorder(root)
+        
+        
         

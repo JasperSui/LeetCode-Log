e2d4d3ea67bee9d0585c72b10b3ce5c4b528d4af
Sui Yang <suiyang03@gmail.com>
2021-05-22 17:09:09 +0800

    Time: 52 ms (38.98%), Space: 15.2 MB (95.31%) - LeetHub

--- populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -10,17 +10,34 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        self.d = defaultdict(list)
-        def dfs(node, level=0):
-            if not node: return
-            self.d[level].append(node)
-            dfs(node.left, level+1)
-            dfs(node.right, level+1)
-        dfs(root)
-        for l in self.d.values():
-            for i in range(len(l)):
-                if i == len(l) - 1: l[i].next = None
-                else:
-                    l[i].next = l[i+1]
+#         self.d = defaultdict(list)
+#         def dfs(node, level=0):
+#             if not node: return
+#             self.d[level].append(node)
+#             dfs(node.left, level+1)
+#             dfs(node.right, level+1)
+#         dfs(root)
+#         for l in self.d.values():
+#             for i in range(len(l)):
+#                 if i == len(l) - 1: l[i].next = None
+#                 else:
+#                     l[i].next = l[i+1]
+#         return root
+        
+        if not root: return
+        curr = root
+        p = dummy = Node()
+        while curr:
+            if curr.left:
+                p.next = curr.left
+                p = p.next
+            if curr.right:
+                p.next = curr.right
+                p = p.next
+            if curr.next:
+                curr = curr.next
+            else:
+                curr = dummy.next
+                dummy.next = None
+                p = dummy
         return root
-                

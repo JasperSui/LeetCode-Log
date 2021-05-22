e49af067eb22df77a7ea53bb327057900cac4351
Sui Yang <suiyang03@gmail.com>
2021-05-22 14:33:32 +0800

    Time: 40 ms (91.18%), Space: 14.5 MB (9.10%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -10,9 +10,16 @@ class Solution:
         # return low
         
         # Solution 2
+        # low, high = 0, len(nums)-1
+        # while low < high:
+        #     mid = (low+high) // 2
+        #     if nums[mid] < nums[mid+1]: low = mid + 1
+        #     else: high = mid
+        # return low
+        
         low, high = 0, len(nums)-1
         while low < high:
-            mid = (low+high) // 2
+            mid = low + (high-low) // 2
             if nums[mid] < nums[mid+1]: low = mid + 1
             else: high = mid
         return low

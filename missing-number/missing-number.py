cb51197180c26e6594306e494ae772ea715a405d
Sui Yang <suiyang03@gmail.com>
2021-05-26 12:44:35 +0800

    Time: 132 ms (58.99%), Space: 15.5 MB (50.77%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -4,10 +4,21 @@ class Solution:
         # return sum(range(len(nums)+1)) - sum(nums)
     
         # Solution 2
-        len_n = len(nums)
-        res = 0 ^ len_n
-        for i in range(len(nums)):
-            res ^= i ^ nums[i]
-        return res
+        # len_n = len(nums)
+        # res = 0 ^ len_n
+        # for i in range(len(nums)):
+        #     res ^= i ^ nums[i]
+        # return res
     
+        # Solution 3
+        # len_n = len(nums)
+        # res = len_n
+        # for i in range(len(nums)):
+        #     res += i - nums[i]
+        # return res
         
+        res = len(nums)
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res        
+    

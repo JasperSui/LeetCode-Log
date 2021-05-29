d7c7af485c4d21918dc26e1b64ed91a6b640a9d0
Sui Yang <suiyang03@gmail.com>
2021-05-29 22:20:34 +0800

    Time: 100 ms (27.79%), Space: 14.3 MB (90.00%) - LeetHub

---
+++ permutations-ii/permutations-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        self.res = [nums]
+        temp = nums.copy()
+        while True:
+            temp = self.get_next_permutation(temp.copy())
+            if temp and temp not in self.res:
+                self.res.append(temp.copy())
+            else:
+                break
+        return self.res
+        
+    def get_next_permutation(self, nums):
+        i = j = len(nums) - 1
+        while i > 0 and nums[i] <= nums[i-1]:
+            i -= 1
+        if i == 0:
+            nums.reverse()
+            return nums
+        k = i-1
+        while j > 0 and nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]
+        l, r = k+1, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        return nums

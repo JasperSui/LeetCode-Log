5c5208fb2866afae38424efe487be56bddb61c5d
Sui Yang <suiyang03@gmail.com>
2021-05-31 12:58:12 +0800

    Time: 60 ms (99.47%), Space: 18.4 MB (59.40%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.py
+++ delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -7,21 +7,44 @@
 class Solution:
     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
         if not root: return
-        if root.val > key: 
+        if root.val > key:
             root.left = self.deleteNode(root.left, key)
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
-        else: # root.val == key
+        else:
             if not root.left:
                 return root.right
             if not root.right:
                 return root.left
-            # left and right are both not null
             temp = root.right
             min_v = temp.val
-            while temp and temp.left: 
+            while temp and temp.left:
                 temp = temp.left
                 min_v = temp.val
             root.val = min_v
             root.right = self.deleteNode(root.right, root.val)
         return root
+        
+        
+        
+        
+        
+        # if not root: return
+        # if root.val > key: 
+        #     root.left = self.deleteNode(root.left, key)
+        # elif root.val < key:
+        #     root.right = self.deleteNode(root.right, key)
+        # else: # root.val == key
+        #     if not root.left:
+        #         return root.right
+        #     if not root.right:
+        #         return root.left
+        #     # left and right are both not null
+        #     temp = root.right
+        #     min_v = temp.val
+        #     while temp and temp.left: 
+        #         temp = temp.left
+        #         min_v = temp.val
+        #     root.val = min_v
+        #     root.right = self.deleteNode(root.right, root.val)
+        # return root

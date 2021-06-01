43469cf5cadcdc8bcac395a59b083844d009496d
Sui Yang <suiyang03@gmail.com>
2021-06-01 13:36:25 +0800

    Time: 48 ms (94.97%), Space: 18.1 MB (70.05%) - LeetHub

--- find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -8,23 +8,27 @@ class Solution:
     def findMode(self, root: TreeNode) -> List[int]:
         self.res = []
         self.curr_val = 0
-        self.max_count = 0
         self.curr_count = 0
+        self.max_count = 0
+        
         def inorder(node):
             if not node: return
+            
             inorder(node.left)
-
-            if node.val != self.curr_val:
+            
+            if self.curr_val != node.val:
                 self.curr_val = node.val
                 self.curr_count = 0
-            
+
             self.curr_count += 1
+            
             if self.curr_count > self.max_count:
                 self.max_count = self.curr_count
-                self.res = [node.val]
+                self.res = [self.curr_val]
             elif self.curr_count == self.max_count:
-                self.res.append(node.val)
+                self.res += [self.curr_val]
+            
             inorder(node.right)
+        
         inorder(root)
         return self.res
-        

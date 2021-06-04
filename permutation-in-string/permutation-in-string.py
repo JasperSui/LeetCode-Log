6d00d8da37fadbb83beffe0561613d0498324c55
Sui Yang <suiyang03@gmail.com>
2021-06-04 13:00:18 +0800

    Time: 64 ms (86.36%), Space: 14.4 MB (61.05%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -1,15 +1,11 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        need = defaultdict(int)
-        window = defaultdict(int)
-        left, right, valid = 0, 0, 0
-        
+        need, window = defaultdict(int), defaultdict(int)
         for c in s1: need[c] += 1
-
+        left, right, valid = 0, 0, 0
         while right < len(s2):
             c = s2[right]
             right += 1
-            
             if c in need:
                 window[c] += 1
                 if window[c] == need[c]:

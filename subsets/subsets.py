e538218a3052707195b92e4eacfd1e5599a4295d
Sui Yang <suiyang03@gmail.com>
2021-06-06 16:05:56 +0800

    Time: 52 ms (6.74%), Space: 14.4 MB (49.18%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,11 +1,32 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.res = []
-        def backtrack(temp, start):
-            self.res.append(temp.copy())
-            for i in range(start, len(nums)):
-                backtrack(temp + [nums[i]], i + 1)
-        backtrack([], 0)
-        return self.res
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        res.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], path + [nums[i]], res)
+        
+        
+        # self.res = []
+        # def backtrace(temp, start):
+        #     self.res.append(temp.copy())
+        #     for i in range(start, len(nums)):
+        #         backtrace(temp + [nums[i]], i+1)
+        # backtrace([], 0)
+        # return self.res
+        
+        
+        
+        
+#         self.res = []
+#         def backtrack(temp, start):
+#             self.res.append(temp.copy())
+#             for i in range(start, len(nums)):
+#                 backtrack(temp + [nums[i]], i + 1)
+#         backtrack([], 0)
+#         return self.res
     
     

04717aaa6f34444e07d4bdb919070a00925859f9
Sui Yang <suiyang03@gmail.com>
2021-06-09 12:55:16 +0800

    Time: 208 ms (48.68%), Space: 15.5 MB (72.51%) - LeetHub

--- set-mismatch/set-mismatch.py
+++ set-mismatch/set-mismatch.py
@@ -4,13 +4,22 @@ class Solution:
         # Space: O(1)
         
         dup = -1
-        
-        # dup
         for n in nums:
             if nums[abs(n)-1] < 0: dup = abs(n)
             else: nums[abs(n)-1] *= -1
         
-        # missing
         for i in range(len(nums)):
             if nums[i] > 0: return [dup, i + 1]
+        
+        
+#         dup = -1
+        
+#         # dup
+#         for n in nums:
+#             if nums[abs(n)-1] < 0: dup = abs(n)
+#             else: nums[abs(n)-1] *= -1
+        
+#         # missing
+#         for i in range(len(nums)):
+#             if nums[i] > 0: return [dup, i + 1]
         

218acd480c791140b3444791bd551ee6655dacd8
Sui Yang <suiyang03@gmail.com>
2021-06-11 12:51:17 +0800

    Time: 88 ms (17.13%), Space: 16.3 MB (19.31%) - LeetHub

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -6,15 +6,35 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root: return
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        
         # Time: O(N)
         # Space: O(height)
-        # Iterable
-        stack = [root]
-        while stack:
-            curr = stack.pop()
-            if not curr: return None
-            if curr.val == val: return curr
-            elif curr.val > val:
-                stack.append(curr.left)
-            else:
-                stack.append(curr.right)
+        
+        # # Iterative
+        # stack = [root]
+        # while stack:
+        #     curr = stack.pop()
+        #     if not curr: return None
+        #     if curr.val == val: return curr
+        #     elif curr.val > val:
+        #         stack.append(curr.left)
+        #     else:
+        #         stack.append(curr.right)
+        
+        # # Recursive
+        def dfs(node):
+            if not node: return
+            if node.val == val:
+                return node
+            elif node.val > val:
+                dfs(node.left)
+            else: 
+                dfs(node.right)
+        return dfs(root)

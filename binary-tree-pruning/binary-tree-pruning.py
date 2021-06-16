c4d7627b94ee20788ce64fa4f2f61ae90b33ecdf
Sui Yang <suiyang03@gmail.com>
2021-06-16 12:34:41 +0800

    Time: 28 ms (85.55%), Space: 14.4 MB (22.26%) - LeetHub

--- binary-tree-pruning/binary-tree-pruning.py
+++ binary-tree-pruning/binary-tree-pruning.py
@@ -6,6 +6,56 @@
 #         self.right = right
 class Solution:
     def pruneTree(self, root: TreeNode) -> TreeNode:
+        
+        def dfs(node):
+            if not node: return
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+            if not node.val and not node.left and not node.right: return
+            return node
+        return dfs(root)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # Time: O(n)
         # Space: O(height)
         
@@ -17,21 +67,16 @@ class Solution:
         # return root
         
         # Iterative
-        if not root: return root
-        fr = TreeNode(1) # Fake root
-        fr.left = root
-        stack = [(0, root, fr)] # (seen, node, parent)
+        stack = [(0, root)]
         while stack:
-            seen, node, parent = stack.pop()
-            if not node: continue
+            seen, node = stack.pop()
+            if node is None:
+                continue
             if not seen:
-                stack += [(1, node, parent)]
-                stack += [(0, node.left, node)]
-                stack += [(0, node.right, node)]
+                stack.extend([(1, node), (0, node.right), (0, node.left)])
             else:
-                if node.val == 0 and not node.left and not node.right:
-                    if parent.left == node:
-                        parent.left = None
-                    else:
-                        parent.right = None
-        return fr.left
+                if node.left and not (node.left.val or node.left.left or node.left.right):
+                    node.left = None
+                if node.right and not (node.right.val or node.right.left or node.right.right):
+                    node.right = None
+        return root

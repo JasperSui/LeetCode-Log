34b9a56e51f5dc91374ad76b88c4b03d994ae1b6
Sui Yang <suiyang03@gmail.com>
2021-06-17 12:50:55 +0800

    Time: 32 ms (66.09%), Space: 14.2 MB (89.82%) - LeetHub

--- increasing-order-search-tree/increasing-order-search-tree.py
+++ increasing-order-search-tree/increasing-order-search-tree.py
@@ -5,17 +5,69 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.head, self.prev = None, None
-    def increasingBST(self, root: TreeNode) -> TreeNode:
-        if not root: return
-        self.increasingBST(root.left)
-        if self.prev:
-            root.left = None
-            self.prev.right = root
-        if not self.head:
-            self.head = root
-        self.prev = root
-        self.increasingBST(root.right)
-        return self.head
+    def increasingBST(self, root: TreeNode, tail=None) -> TreeNode:
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        print(res)
+        print(root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
+    
+#     def __init__(self):
+#         self.head, self.prev = None, None
+#     def increasingBST(self, root: TreeNode) -> TreeNode:
+#         if not root: return
+#         self.increasingBST(root.left)
+#         if self.prev:
+#             root.left = None
+#             self.prev.right = root
+#         if not self.head:
+#             self.head = root
+#         self.prev = root
+#         self.increasingBST(root.right)
+#         return self.head
         

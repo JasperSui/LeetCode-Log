9b35d07e2eb581329dabe35aadf18de1ca13dd1b
Sui Yang <suiyang03@gmail.com>
2021-06-19 17:01:40 +0800

    Time: 84 ms (77.54%), Space: 16.1 MB (53.62%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,13 +1,39 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        curr = 0
-        intervals.sort(key=lambda x: x[0])
-        while curr < len(intervals)-1:
-            curr_1, curr_2 = intervals[curr]
-            next_1, next_2 = intervals[curr+1]
-            if curr_2 >= next_1:
-                intervals[curr] = [min(curr_1, next_1), max(curr_2, next_2)]
-                del intervals[curr+1]
+        out = []
+        for start, end in sorted(intervals, key=lambda x: x[0]):
+            if out and out[-1][1] >= start:
+                out[-1][1] = max(out[-1][1], end)
             else:
-                curr += 1
-        return intervals
+                out += [[start, end]]
+        return out
+        
+        
+        
+        # curr = 0
+        # intervals.sort(key=lambda x: x[0])
+        # while curr < len(intervals)-1:
+        #     curr_1, curr_2 = intervals[curr]
+        #     next_1, next_2 = intervals[curr+1]
+        #     if curr_2 >= next_1:
+        #         intervals[curr] = [min(curr_1, next_1), max(curr_2, next_2)]
+        #         del intervals[curr+1]
+        #     else:
+        #         curr += 1
+        # return intervals
+        
+        # out = []
+        # for start, end in sorted(intervals, key=lambda x: x[0]):
+        #     if out and start <= out[-1][1]:
+        #         out[-1][1] = max(out[-1][1], end)
+        #     else:
+        #         out += [[start, end]]
+        # return out
+    
+        # out = []
+        # for i in sorted(intervals, key=lambda x: x[0]):
+        #     if out and i[0] <= out[-1][1]:
+        #         out[-1][1] = max(out[-1][1], i[1])
+        #     else:
+        #         out += [i]
+        # return out

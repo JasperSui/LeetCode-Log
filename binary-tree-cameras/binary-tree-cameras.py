c936f158a87f65482ae4b0ebd260bd0f66f84c0a
Sui Yang <suiyang03@gmail.com>
2021-06-21 13:15:06 +0800

    Time: 44 ms (67.85%), Space: 14.7 MB (15.55%) - LeetHub

--- binary-tree-cameras/binary-tree-cameras.py
+++ binary-tree-cameras/binary-tree-cameras.py
@@ -6,18 +6,36 @@
 #         self.right = right
 class Solution:
     def minCameraCover(self, root: TreeNode) -> int:
+        leaf = 0
+        camera = 1
+        covered = 2
+        self.ans = 0
+        if not root: return 0
+        def dfs(node):
+            if not node: return covered
+            l, r = dfs(node.left), dfs(node.right)
+            if l == leaf or r == leaf:
+                self.ans += 1
+                return camera
+            return covered if l == camera or r == camera else 0
+        return (dfs(root) == 0) + self.ans
+        
+        
         # Time: O(n)
         # Space: O(height)
         
         # Recursive
         # Append the camera only on the leaves' parent
-        if not root: return 0
-        self.ans = 0
-        def dfs(node):
-            if not node: return 2
-            l, r = dfs(node.left), dfs(node.right)
-            if l == 0 or r == 0:
-                self.ans += 1
-                return 1
-            return 2 if l == 1 or r == 1 else 0
-        return (dfs(root) == 0) + self.ans
+        # if not root: return 0
+        # leaf = 0
+        # camera = 1 # parent of leaf
+        # covered = 2
+        # self.ans = 0
+        # def dfs(node):
+        #     if not node: return covered
+        #     l, r = dfs(node.left), dfs(node.right)
+        #     if l == leaf or r == leaf:
+        #         self.ans += 1
+        #         return camera
+        #     return covered if l == camera or r == camera else 0
+        # return (dfs(root) == 0) + self.ans

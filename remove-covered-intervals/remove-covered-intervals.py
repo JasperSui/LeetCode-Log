98e1d346c3d25e54004e30b4de9267502401570f
Sui Yang <suiyang03@gmail.com>
2021-06-23 20:34:10 +0800

    Time: 88 ms (94.95%), Space: 15.1 MB (5.66%) - LeetHub

--- remove-covered-intervals/remove-covered-intervals.py
+++ remove-covered-intervals/remove-covered-intervals.py
@@ -1,8 +1,8 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         stack = []
-        for start, end in sorted(intervals, key=lambda x:(x[0], -x[1])):
-            if stack and start >= stack[-1][0] and end <= stack[-1][1]:
-                continue
-            stack.append([start, end])
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        for s, e in intervals:
+            if stack and s >= stack[-1][0] and e <= stack[-1][1]: continue
+            stack.append([s, e])
         return len(stack)

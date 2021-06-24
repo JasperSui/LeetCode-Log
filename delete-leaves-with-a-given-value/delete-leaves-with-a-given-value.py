a504211df8900070e44e8646a1bd218f661975c7
Sui Yang <suiyang03@gmail.com>
2021-06-24 21:33:51 +0800

    Time: 48 ms (89.66%), Space: 14.8 MB (27.74%) - LeetHub

--- delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,12 +6,57 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: TreeNode, target: int) -> TreeNode:
+        def dfs(node):
+            if not node: return
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+            if not node.left and not node.right and node.val == target:
+                return None
+            return node
+        return dfs(root)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # Time: O(N)
         # Space: O(height)
         
-        # DFS
-        if not root: return
-        root.left = self.removeLeafNodes(root.left, target)
-        root.right = self.removeLeafNodes(root.right, target)
-        if not root.left and not root.right and root.val == target: return
+        # # DFS
+        # if not root: return
+        # root.left = self.removeLeafNodes(root.left, target)
+        # root.right = self.removeLeafNodes(root.right, target)
+        # if not root.left and not root.right and root.val == target: return
+        # return root
+        
+        # Iterative
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            if curr:
+                stack.append(curr.left)
+                stack.append(curr.right)
+                if not curr.left and not curr.right and curr.val == target:
+                    curr = None
         return root

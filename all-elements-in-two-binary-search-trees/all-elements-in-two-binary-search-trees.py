643f276d01e5aa825eba24efd0ea1e89d12cea1e
Sui Yang <suiyang03@gmail.com>
2021-06-24 12:39:46 +0800

    Time: 364 ms (43.64%), Space: 22.9 MB (10.47%) - LeetHub

--- all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
+++ all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
@@ -21,6 +21,61 @@ class Solution:
                 res.append(self.l2.popleft())
             elif not self.l2:
                 res.append(self.l1.popleft())
+            if self.l1 and self.l2:
+                n1 = self.l1[0]
+                n2 = self.l2[0]
+                if n1 > n2:
+                    res.append(self.l2.popleft())
+                else:
+                    res.append(self.l1.popleft())
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        self.l1 = deque()
+        self.l2 = deque()
+        res = []
+        def dfs(node, l):
+            if not node: return
+            dfs(node.left, l)
+            l.append(node.val)
+            dfs(node.right, l)
+        dfs(root1, self.l1)
+        dfs(root2, self.l2)
+        while self.l1 or self.l2:
+            if not self.l1:
+                res.append(self.l2.popleft())
+            elif not self.l2:
+                res.append(self.l1.popleft())
             elif self.l1 and self.l2:
                 n1 = self.l1[0]
                 n2 = self.l2[0]

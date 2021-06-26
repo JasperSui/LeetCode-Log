6fae6150f3f5cae453e12594861662aefe4d61c9
Sui Yang <suiyang03@gmail.com>
2021-06-26 17:24:10 +0800

    Time: 100 ms (64.06%), Space: 15.8 MB (49.24%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [set() for _ in range(numCourses)]
+        outdegree = [[] for _ in range(numCourses)]
+        for course, pre in prerequisites:
+            indegree[course].add(pre)
+            outdegree[pre].append(course)
+        
+        # start are all nodes without any prerequisites
+        res, start = [], [i for i in range(numCourses) if not indegree[i]]
+        
+        while start:
+            new_start = []
+            for i in start:
+                # append the course first
+                res.append(i)
+                
+                # start to traverse the current courses' outdegree
+                for j in outdegree[i]:
+                    indegree[j].remove(i)
+                    
+                    # Append j to new start because there is no prerequisite anymore
+                    if not indegree[j]:
+                        new_start.append(j)
+            start = new_start
+        return res if len(res) == numCourses else []

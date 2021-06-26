b6349ca35819b77f7be5f0ab314610da39940a38
Sui Yang <suiyang03@gmail.com>
2021-06-26 19:52:02 +0800

    Time: 224 ms (96.88%), Space: 18.7 MB (57.09%) - LeetHub

--- minimum-height-trees/minimum-height-trees.py
+++ minimum-height-trees/minimum-height-trees.py
@@ -2,18 +2,31 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1: return [0]
         adj = [set() for _ in range(n)]
+        
+        # undirected graph
         for i, j in edges:
             adj[i].add(j)
             adj[j].add(i)
+        
+        # eats leaves from outside circally
         leaves = [i for i in range(n) if len(adj[i]) == 1]
+
+        # There will be at most 2 nodes in the center
         while n > 2:
             n -= len(leaves)
             new_leaves = []
             for i in leaves:
+                # Remove the connection between i and j
+                # remove j from adj[i]
                 j = adj[i].pop()
+                
+                # remove i from adj[j]
                 adj[j].remove(i)
+                
+                # if only one node connects j, then j will be the new leave
                 if len(adj[j]) == 1:
                     new_leaves.append(j)
             leaves = new_leaves
+        
+        # Return the rest of leaves, which means the most center node(s)
         return leaves
-            

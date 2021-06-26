fe10c46a3837cd3b75fb74fd3d24e1067f667855
Sui Yang <suiyang03@gmail.com>
2021-06-26 23:52:55 +0800

    Time: 56 ms (6.31%), Space: 14.2 MB (50.90%) - LeetHub

--- xor-operation-in-an-array/xor-operation-in-an-array.py
+++ xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -1,7 +1,32 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
-        result = [start + 2*x for x in range(n)]
+
+        def is_even(i):
+            return i % 2 == 0
+
+        def is_odd(i):
+            return i % 2 == 1
+
         ans = 0
-        for i in result:
-            ans = ans ^ i
+
+        # if n is odd and start is odd, then the last bit of ans will be 1
+        # otherwise, the last bit of ans is 0
+        ans ^= is_odd(n) and is_odd(start)
+
+        # now we can only consider the right-shifted number sequence
+        # consider the case 5, 6, 7, 8, 9, 10
+        # (6, 7) is a pair which produces a 1, same as (8, 9)
+
+        # thus, we need to take care of the 5
+        new_start = start // 2
+        ans ^= new_start << 1 if is_odd(new_start) else 0
+
+        # and we have to take care of the 10
+        new_end = new_start + n - 1
+        ans ^= new_end << 1 if is_even(new_end) else 0
+
+        # finally we have to calculate how many pairs
+        num_pairs = n // 2 - int(is_even(n) and is_odd(new_start))
+        ans ^= is_odd(num_pairs) << 1
+
         return ans

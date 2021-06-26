944f2c9d7964706bbe43d1b2d8d39cd699d6e272
Sui Yang <suiyang03@gmail.com>
2021-06-27 00:19:19 +0800

    Time: 1972 ms (21.23%), Space: 48.7 MB (26.89%) - LeetHub

--- maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
+++ maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
@@ -4,10 +4,12 @@ class Solution:
         count = [0] * (n+1)
         for i, j in requests:
             count[i] += 1
-            count[j+1] -= 1
+            count[j+1] -= 1 # represents the end of request
+        
         for i in range(1, n):
-            count[i] += count[i-1]
-        res = 0
+            count[i] += count[i-1] # it's the frequency of index
+        
+        ans = 0
         for v, c in zip(sorted(count[:-1]), sorted(nums)):
-            res += v*c
-        return res % (10 ** 9 + 7)
+            ans += v*c
+        return ans % (10 ** 9 + 7)

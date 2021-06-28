8419fa789adecb270071719aa127fe4c4fa179fa
Sui Yang <suiyang03@gmail.com>
2021-06-28 11:52:06 +0800

    Time: 140 ms (55.41%), Space: 14.7 MB (80.84%) - LeetHub

--- design-parking-system/design-parking-system.py
+++ design-parking-system/design-parking-system.py
@@ -2,13 +2,12 @@ class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
         self.space = [big, medium, small]
+
     def addCar(self, carType: int) -> bool:
-        if self.space[carType-1] > 0:
+        if self.space[carType-1] < 1: return False
+        else:
             self.space[carType-1] -= 1
             return True
-        else:
-            return False
-        
 
 
 # Your ParkingSystem object will be instantiated and called as such:

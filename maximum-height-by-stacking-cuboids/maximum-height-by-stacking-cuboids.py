aaeea3bd5143fa91b06b0ee1da1d0f74373f4c0e
Sui Yang <suiyang03@gmail.com>
2021-06-28 22:49:05 +0800

    Time: 172 ms (23.30%), Space: 14.5 MB (35.92%) - LeetHub

--- maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.py
+++ maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.py
@@ -1,9 +1,10 @@
 class Solution:
     def maxHeight(self, cuboids: List[List[int]]) -> int:
-        cuboids = [[0,0,0]] + sorted(map(sorted, cuboids))
+        cuboids = [[0, 0, 0]] + sorted(map(sorted, cuboids))
         dp = [0] * len(cuboids)
         for j in range(1, len(cuboids)):
             for i in range(j):
                 if all(cuboids[i][k] <= cuboids[j][k] for k in range(3)):
                     dp[j] = max(dp[j], dp[i] + cuboids[j][2])
         return max(dp)
+        

a4e5118d1e19c0dcb4787a469187ca2c5003602a
Sui Yang <suiyang03@gmail.com>
2021-06-29 22:59:36 +0800

    Time: 60 ms (58.81%), Space: 15.3 MB (30.79%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
-        for i, v in enumerate(nums):
-            if target-v in d: return [d[target-v], i]
-            d[v] = i
-            
+        for i, n in enumerate(nums):
+            if target-n in d: return [i, d[target-n]]
+            d[n] = i

fe18410d46d0d7cae4a07c0298c66baf7e9632e8
Sui Yang <suiyang03@gmail.com>
2021-06-29 22:02:15 +0800

    Time: 40 ms (85.02%), Space: 14.3 MB (36.79%) - LeetHub

--- maximum-population-year/maximum-population-year.py
+++ maximum-population-year/maximum-population-year.py
@@ -1,12 +1,13 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        d = defaultdict(int)
-        c = defaultdict(list)
-        ans = 0
+        l = [0] * 101
         for birth, death in logs:
-            for i in range(birth, death):
-                d[i] += 1
-                if d[i] >= ans:
-                    heapq.heappush(c[d[i]], i)
-                    ans = d[i]
-        return heapq.nsmallest(1, c[ans])[0]
+            l[birth-1950] += 1
+            l[death-1950] -= 1
+        max_num, max_year = l[0], 1950
+        for i in range(1, len(l)):
+            l[i] += l[i-1]
+            if l[i] > max_num:
+                max_num = l[i]
+                max_year = i+1950
+        return max_year

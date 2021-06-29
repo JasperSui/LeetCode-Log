78aeed701938a5fc3ebf6fe90f47a34c341cbbb4
Sui Yang <suiyang03@gmail.com>
2021-06-29 12:47:18 +0800

    Time: 32 ms (98.62%), Space: 14.7 MB (53.45%) - LeetHub

--- closest-dessert-cost/closest-dessert-cost.py
+++ closest-dessert-cost/closest-dessert-cost.py
@@ -1,21 +1,22 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        self.ans = float('inf')
-        toppingCosts.sort()
-        for base in baseCosts:
-            self.dfs(toppingCosts, target, base)
-        return self.ans
-    
-    def dfs(self, topping, target, sums):
-        if abs(target - sums) < abs(target - self.ans):
-            self.ans = sums
-        elif abs(target - sums) == abs(target - self.ans):
-            self.ans = min(self.ans, sums)
-        if sums > target: return
-        if sums == target:
-            self.ans = sums
-            return
-        for i in range(len(topping)):
-            self.dfs(topping[i+1:], target, sums+0*topping[i])
-            self.dfs(topping[i+1:], target, sums+1*topping[i])
-            self.dfs(topping[i+1:], target, sums+2*topping[i])
+        baseCosts.sort(reverse=True)
+        toppingCosts.sort(reverse=True)
+        res = baseCosts[0]
+        res_delta = abs(target - res)
+        for bc in baseCosts:
+            candidates = set([bc])
+            for tc in toppingCosts:
+                new_candidates = set()
+                for c in candidates:
+                    for x in range(3):
+                        nc = c + x*tc
+                        if nc - target < res_delta:
+                            new_candidates.add(nc)
+                        delta = abs(nc - target)
+                        if delta < res_delta or (delta == res_delta and nc < res):
+                            res = nc
+                            res_delta = delta
+                candidates = new_candidates
+        return res
+                        

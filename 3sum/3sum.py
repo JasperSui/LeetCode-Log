d5fcb21343fb88208f1688cec66167f152522567
Sui Yang <suiyang03@gmail.com>
2021-06-30 22:31:34 +0800

    Time: 836 ms (76.49%), Space: 17.5 MB (72.77%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,20 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3: return []
         res = []
         nums.sort()
         for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]: continue
-            front, back = i+1, len(nums)-1
-            while front < back:
-                s = nums[i] + nums[front] + nums[back]
-                if s < 0: front += 1
-                elif s > 0: back -= 1
-                else:
-                    res.append([nums[i], nums[front], nums[back]])
-                    while front < back and nums[front] == nums[front+1]: front += 1
-                    while front < back and nums[back] == nums[back-1]: back -= 1
+            front, end = i+1, len(nums)-1
+            while front < end:
+                s = nums[i] + nums[front] + nums[end]
+                if s > 0:
+                    end -= 1
+                elif s < 0:
                     front += 1
-                    back -= 1
+                elif s == 0:
+                    res.append([nums[i], nums[front], nums[end]])
+
+                    while front < end and nums[front] == nums[front+1]: front += 1
+                    while front < end and nums[end] == nums[end-1]: end -= 1
+                    front += 1
+                    end -= 1
         return res
-            

7f34dcdbfe7ff95550edd8550c064d171ca39e0f
Sui Yang <suiyang03@gmail.com>
2021-07-01 23:54:21 +0800

    Time: 100 ms (78.02%), Space: 17.9 MB (63.88%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.py
+++ merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -10,10 +10,11 @@ class Solution:
         for l in lists:
             if l:
                 heapq.heappush(pq, (l.val, id(l), l))
-        
+
         while pq:
             _, _, curr.next = heapq.heappop(pq)
             curr = curr.next
             if curr.next:
                 heapq.heappush(pq, (curr.next.val, id(curr.next), curr.next))
         return head.next
+            

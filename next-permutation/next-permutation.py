003741705c157abc41c743cd0dfcf4a6d4593f31
Sui Yang <suiyang03@gmail.com>
2021-07-02 00:10:44 +0800

    Time: 40 ms (78.95%), Space: 14 MB (92.41%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,17 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Find the longest decresing suffix
         i = j = len(nums)-1
         while i > 0 and nums[i] <= nums[i-1]:
             i -= 1
+        
+        # Reverse if nums is a decreasing list
         if i == 0:
             nums.reverse()
             return
+        
+        # Pivot
         k = i - 1
+        
+        # Find the element greater than pivot
         while j > 0 and nums[j] <= nums[k]:
             j -= 1
+            
+        # Switch
         nums[j], nums[k] = nums[k], nums[j]
         
+        # Reverse the rest from k+1 to last element( len(nums)-1 )
         low, high = k+1, len(nums) - 1
         while low < high:
             nums[low], nums[high] = nums[high], nums[low]

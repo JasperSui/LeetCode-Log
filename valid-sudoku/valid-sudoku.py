a851c085a8d319102869f4973014cb9a1d4cfb72
Sui Yang <suiyang03@gmail.com>
2021-07-02 00:22:28 +0800

    Time: 96 ms (71.36%), Space: 14.4 MB (13.80%) - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,22 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
+        def is_valid(units):
+            units = [u for u in units if u != "."]
+            return len(set(units)) == len(units)
+
         for row in board:
-            if not self.is_valid(row):
-                return False
+            if not is_valid(row): return False
         
         for col in zip(*board):
-            if not self.is_valid(col):
-                return False
+            if not is_valid(col): return False
         
         for i in (0, 3, 6):
             for j in (0, 3, 6):
                 units = [board[a][b] for a in range(i, i+3) for b in range(j, j+3)]
-                if not self.is_valid(units):
-                    return False
-        return True
+                if not is_valid(units): return False
         
-    
-    def is_valid(self, units):
-        units = [u for u in units if u != "."]
-        return len(units) == len(set(units))
+        return True

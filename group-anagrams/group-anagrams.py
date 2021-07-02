089d7446805607d82ae8002adfe99f36f326adb2
Sui Yang <suiyang03@gmail.com>
2021-07-02 22:16:20 +0800

    Time: 84 ms (99.00%), Space: 18.3 MB (26.31%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -2,6 +2,5 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(list)
         for s in strs:
-            key = tuple(sorted(s))
-            d[key].append(s)
+            d[tuple(sorted(s))].append(s)
         return d.values()

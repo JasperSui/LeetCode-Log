9822e2eef19ebb341427f1c160d33faa67af7ba7
Sui Yang <suiyang03@gmail.com>
2021-07-02 12:14:39 +0800

    Time: 92 ms (43.99%), Space: 14.4 MB (45.73%) - LeetHub

--- combination-sum-ii/combination-sum-ii.py
+++ combination-sum-ii/combination-sum-ii.py
@@ -1,16 +1,17 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
         res = []
-        candidates.sort()
-        self.dfs(candidates, target, 0, [], res)
+        nums.sort()
+        self.dfs(nums, target, 0, [], res)
         return res
     
     def dfs(self, nums, target, index, path, res):
-        if target < 0:
-            return
-        if target == 0:
+        if target < 0: return
+        elif target == 0:
             res.append(path)
+            return
         else:
             for i in range(index, len(nums)):
-                if i > index and nums[i] == nums[i-1]: continue
-                self.dfs(nums, target-nums[i], i+1, path + [nums[i]], res)
+                if i > index and nums[i] == nums[i-1]:
+                    continue
+                self.dfs(nums, target-nums[i], i+1, path+[nums[i]], res)

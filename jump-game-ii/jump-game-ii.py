57c3aba8115dde033a265f815ecf4766fc2d6689
Sui Yang <suiyang03@gmail.com>
2021-07-02 17:44:43 +0800

    Time: 116 ms (35.47%), Space: 15.3 MB (13.86%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -1,5 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        if len(nums) < 2: return 0
+        steps = curr_far = curr_end = 0
+        for i in range(len(nums) - 1):
+            curr_far = max(curr_far, i + nums[i])
+            if i == curr_end:
+                steps += 1
+                curr_end = curr_far
+        return steps
+        
+        
+        
+        
         # target = len(nums)-1
         # step = 0
         # curr_index = 0

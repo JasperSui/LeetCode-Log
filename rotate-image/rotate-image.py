79419fdbab19e9e9e6a5b398d523813310fee754
Sui Yang <suiyang03@gmail.com>
2021-07-02 22:13:52 +0800

    Time: 32 ms (86.15%), Space: 14.4 MB (5.05%) - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -5,5 +5,5 @@ class Solution:
         """
         matrix.reverse()
         for i in range(len(matrix)):
-            for j in range(i+1, len(matrix[i])):
+            for j in range(i, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]

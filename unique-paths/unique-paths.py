7f4fde8f3a49dbbca0ac9a1699489c3fb7dd5444
Sui Yang <suiyang03@gmail.com>
2021-07-04 16:29:56 +0800

    Time: 28 ms (87.07%), Space: 14.4 MB (36.94%) - LeetHub

--- unique-paths/unique-paths.py
+++ unique-paths/unique-paths.py
@@ -1,16 +1,7 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int, dp={}) -> int:
-        # Time: O(mn)
-        # Space: O(m+n)
-        if f'{m},{n}' in dp: return dp[f'{m},{n}']
-        if m == 1 and n == 1: return 1
-        if m == 0 or n == 0: return 0
-        dp[f'{m},{n}'] = self.uniquePaths(m-1, n, dp) + self.uniquePaths(m, n-1, dp)
-        return dp[f'{m},{n}']
-        
-        # aux = [[1 for i in range(n)] for i in range(m)]
-        # for i in range(1, m):
-        #     for j in range(1, n):
-        #         aux[i][j] = aux[i][j-1] + aux[i-1][j]
-        # return aux[-1][-1]
-    
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]

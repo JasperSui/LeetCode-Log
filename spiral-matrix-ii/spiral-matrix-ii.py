eb63f9cefe1e5b283b062e4dd970a993960709ef
Sui Yang <suiyang03@gmail.com>
2021-07-05 12:22:04 +0800

    Time: 32 ms (66.61%), Space: 14.4 MB (44.86%) - LeetHub

--- spiral-matrix-ii/spiral-matrix-ii.py
+++ spiral-matrix-ii/spiral-matrix-ii.py
@@ -1,14 +1,41 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        res = [[0]*n for _ in range(n)]
-        i, j, di, dj = 0, 0, 0, 1
-        for k in range(n*n):
-            res[i][j] = k + 1
-            if res[(i + di) % n][(j + dj) % n]:
-                di, dj = dj, -di
-            i += di
-            j += dj
+        res = [[0] * n for _ in range(n)]
+        row_start, row_end, col_start, col_end = 0, n-1, 0, n-1
+        count = 1
+        while row_start <= row_end and col_start <= col_end:
+            for i in range(col_start, col_end+1):
+                res[row_start][i] = count
+                count += 1
+            row_start += 1
+            
+            for i in range(row_start, row_end+1):
+                res[i][col_end] = count
+                count += 1
+            col_end -= 1
+            
+            if row_start <= row_end:
+                for i in range(col_end, col_start-1, -1):
+                    res[row_end][i] = count
+                    count += 1
+            row_end -= 1
+            
+            if col_start <= col_end:
+                for i in range(row_end, row_start-1, -1):
+                    res[i][col_start] = count
+                    count += 1
+            col_start += 1
         return res
+        
+        # res = [[0]*n for _ in range(n)]
+        # i, j, di, dj = 0, 0, 0, 1
+        # for k in range(n*n):
+        #     res[i][j] = k + 1
+        #     if res[(i + di) % n][(j + dj) % n]:
+        #         di, dj = dj, -di
+        #     i += di
+        #     j += dj
+        # return res
         
 #         res = [[0]*n for i in range(n)]
 #         row_s, row_e, col_s, col_e = 0, n-1, 0, n-1

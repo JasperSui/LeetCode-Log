04fceb8a146a3f1ff6e8f6e62e43e76acca158aa
Sui Yang <suiyang03@gmail.com>
2021-07-05 12:08:38 +0800

    Time: 76 ms (87.18%), Space: 17.7 MB (31.18%) - LeetHub

--- insert-interval/insert-interval.py
+++ insert-interval/insert-interval.py
@@ -1,9 +1,9 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         new_left, new_right = newInterval
-        left_part = [[l, r] for [l, r] in intervals if r < new_left]
-        right_part = [[l, r] for [l, r] in intervals if l > new_right]
-        if left_part + right_part != intervals:
-            new_left = min(new_left, intervals[len(left_part)][0])
-            new_right = max(new_right, intervals[~len(right_part)][1])
-        return left_part + [[new_left, new_right]] + right_part
+        left = [[l, r] for l, r in intervals if r < new_left]
+        right = [[l, r] for l, r in intervals if l > new_right]
+        if left + right != intervals:
+            new_left = min(new_left, intervals[len(left)][0])
+            new_right = max(new_right, intervals[len(intervals)-len(right)-1][1])
+        return left + [[new_left, new_right]] + right

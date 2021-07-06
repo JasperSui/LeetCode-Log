c6386184390ecd748b10b626b0a5049996f3c016
Sui Yang <suiyang03@gmail.com>
2021-07-07 00:17:28 +0800

    Time: 36 ms (37.10%), Space: 14.3 MB (65.11%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -2,7 +2,7 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s or s[0] == "0": return 0
         
-        dp = [0] * (len(s) + 1)
+        dp = [0] * (len(s)+1)
         dp[0], dp[1] = 1, 1
         for i in range(2, len(s)+1):
             if 0 < int(s[i-1:i]):

cfd5b6b12c8002363779f62a1bf3737d573f1293
Sui Yang <suiyang03@gmail.com>
2021-07-06 12:36:44 +0800

    Time: 92 ms (90.47%), Space: 14.9 MB (64.07%) - LeetHub

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -1,17 +1,19 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         need, window = defaultdict(int), defaultdict(int)
-        for c in t: need[c] += 1
-        
+        for c in t:
+            need[c] += 1
         left, right, valid = 0, 0, 0
         start, start_len = 0, float('inf')
+        
         while right < len(s):
             c = s[right]
             right += 1
+            
             if c in need:
                 window[c] += 1
-                
-                if window[c] == need[c]: valid += 1
+                if window[c] == need[c]:
+                    valid += 1
             
             while valid == len(need):
                 if right - left < start_len:
@@ -20,8 +22,8 @@ class Solution:
                 d = s[left]
                 left += 1
                 if d in need:
-                    if window[d] == need[d]: valid -= 1
+                    if window[d] == need[d]:
+                        valid -= 1
                     window[d] -= 1
         if start_len == float('inf'): return ""
-        else: return s[start:start+start_len]
-            
+        return s[start:start+start_len]

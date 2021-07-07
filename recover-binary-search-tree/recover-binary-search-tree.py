f0e5d46f034d50fd2328770525ce950d3f700988
Sui Yang <suiyang03@gmail.com>
2021-07-07 13:07:32 +0800

    Time: 84 ms (24.99%), Space: 14.7 MB (20.64%) - LeetHub

--- recover-binary-search-tree/recover-binary-search-tree.py
+++ recover-binary-search-tree/recover-binary-search-tree.py
@@ -11,15 +11,16 @@ class Solution:
         """
         self.first_node, self.second_node = None, None
         self.prev = TreeNode(float('-inf'))
-        def inorder(node):
+        def dfs(node):
             if not node: return
-            inorder(node.left)
+            dfs(node.left)
             if self.prev.val >= node.val:
-                if self.first_node is None:
+                if not self.first_node:
                     self.first_node = self.prev
                 if self.first_node:
                     self.second_node = node
-            self.prev = node
-            inorder(node.right)
-        inorder(root)
+            else:
+                self.prev = node
+            dfs(node.right)
+        dfs(root)
         self.first_node.val, self.second_node.val = self.second_node.val, self.first_node.val

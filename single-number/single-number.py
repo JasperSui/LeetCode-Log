2f5b35554f3eabbf3edbb35a4829779c4e9a1f82
Sui Yang <suiyang03@gmail.com>
2021-07-09 22:11:31 +0800

    Time: 124 ms (93.61%), Space: 16.7 MB (19.23%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a = set()
-        b = set()
+        ans = 0
         for n in nums:
-            if n not in a and n not in b: a.add(n)
-            elif n in a and n not in b: 
-                a.remove(n)
-                b.add(n)
-        return a.pop()
+            ans ^= n
+        return ans

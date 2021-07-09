543677ccebf7630fcd2855f32f4430b1bee56710
Sui Yang <suiyang03@gmail.com>
2021-07-09 13:26:58 +0800

    Time: 1224 ms (54.18%), Space: 28.3 MB (57.05%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,5 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        dp_i_1_0, dp_i_1_1 = 0, float('-inf')
+        dp_i_2_0, dp_i_2_1 = 0, float('-inf')
+        for p in prices:
+            dp_i_2_0 = max(dp_i_2_0, dp_i_2_1 + p)
+            dp_i_2_1 = max(dp_i_2_1, dp_i_1_0 - p)
+            dp_i_1_0 = max(dp_i_1_0, dp_i_1_1 + p)
+            dp_i_1_1 = max(dp_i_1_1, - p)
+        return dp_i_2_0
+        
+        
         # buy1, buy2 = float('-inf'), float('-inf')
         # sell1, sell2 = 0, 0
         # for p in prices:

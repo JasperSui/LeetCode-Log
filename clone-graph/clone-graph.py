ebe1c9ebfb204b57f46f44085483b1a3ee753be5
Sui Yang <suiyang03@gmail.com>
2021-07-09 21:58:26 +0800

    Time: 56 ms (9.72%), Space: 14.7 MB (56.04%) - LeetHub

--- clone-graph/clone-graph.py
+++ clone-graph/clone-graph.py
@@ -8,17 +8,71 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        
-        # DFS
         if not node: return
-        # m[node] == cloned graph
-        m = {node: Node(node.val)}
-        stack = [node]
-        while stack:
-            n = stack.pop()
-            for neigh in n.neighbors:
+        def dfs(_node, m):
+            for neigh in _node.neighbors:
                 if neigh not in m:
-                    stack.append(neigh)
                     m[neigh] = Node(neigh.val)
-                m[n].neighbors.append(m[neigh])
+                    dfs(neigh, m)
+                m[_node].neighbors.append(m[neigh])
+        m = {node: Node(node.val)}
+        dfs(node, m)
         return m[node]
+        
+        
+        # # DFS (recursive)
+        # if not node: return
+        # def dfs(_node, m):
+        #     for neigh in _node.neighbors:
+        #         if neigh not in m:
+        #             m[neigh] = Node(neigh.val)
+        #             dfs(neigh, m)
+        #         m[_node].neighbors.append(m[neigh])
+        # m = {node: Node(node.val)}
+        # dfs(node, m)
+        # return m[node]
+        
+        
+        
+        # # BFS
+        # if not node: return
+        # m = {node: Node(node.val)}
+        # queue = deque([node])
+        # while queue:
+        #     n = queue.popleft()
+        #     for neigh in n.neighbors:
+        #         if neigh not in m:
+        #             # Append the node should be traverse soon
+        #             queue.append(neigh)
+        #             m[neigh] = Node(neigh.val)
+        #         m[n].neighbors.append(m[neigh])
+        # return m[node]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # # DFS
+        # if not node: return
+        # # m[node] == cloned graph
+        # m = {node: Node(node.val)}
+        # stack = [node]
+        # while stack:
+        #     n = stack.pop()
+        #     for neigh in n.neighbors:
+        #         if neigh not in m:
+        #             stack.append(neigh)
+        #             m[neigh] = Node(neigh.val)
+        #         m[n].neighbors.append(m[neigh])
+        # return m[node]

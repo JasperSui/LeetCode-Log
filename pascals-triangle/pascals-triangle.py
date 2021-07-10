935db8b091de6fef2631f6168da70a804a715211
Sui Yang <suiyang03@gmail.com>
2021-07-11 01:27:11 +0800

    Time: 32 ms (58.60%), Space: 14.5 MB (23.95%) - LeetHub

--- pascals-triangle/pascals-triangle.py
+++ pascals-triangle/pascals-triangle.py
@@ -1,7 +1,6 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         dp = [[1]*i for i in range(1, numRows+1)]
-        if numRows <= 2: return dp[:numRows]
         for i in range(2, numRows):
             for j in range(i+1):
                 if not (j == 0 or j == i):

f7ac6e0dcf3117fa3d9816753fa94cc04cf87674
Sui Yang <suiyang03@gmail.com>
2021-07-11 12:59:01 +0800

    Time: 100 ms (39.89%), Space: 14.4 MB (40.96%) - LeetHub

---
+++ number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        # https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/discuss/574923/JavaC%2B%2BPython-DP-O(1)-Space
+        a121, a123, mod = 6, 6, 10 ** 9 + 7
+        for _ in range(n-1):
+            a121, a123 = a121*3+a123*2, a121*2+a123*2
+        return (a121+a123) % mod

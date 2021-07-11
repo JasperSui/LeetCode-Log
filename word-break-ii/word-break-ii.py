bcb1f8934a9bf8a52405079fffaa322a785b29ce
Sui Yang <suiyang03@gmail.com>
2021-07-12 00:40:24 +0800

    Time: 40 ms (13.27%), Space: 14.3 MB (36.45%) - LeetHub

---
+++ word-break-ii/word-break-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        return self.helper(s, wordDict, {})
+    
+    def helper(self, s, wordDict, memo):
+        if s in memo: return memo[s]
+        if not s: return []
+        
+        res = []
+        for word in wordDict:
+            if not s.startswith(word):
+                continue
+            if len(word) == len(s):
+                res.append(word)
+            else:
+                rest_list = self.helper(s[len(word):], wordDict, memo)
+                for item in rest_list:
+                    item = f"{word} {item}"
+                    res.append(item)
+        memo[s] = res
+        return res

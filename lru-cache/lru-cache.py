84cc65b88861fb71e76cdaa8fb2cac84a45b1168
Sui Yang <suiyang03@gmail.com>
2021-07-12 12:44:22 +0800

    Time: 1256 ms (9.25%), Space: 74.3 MB (31.29%) - LeetHub

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,47 +1,54 @@
 class Node:
     def __init__(self, k, v):
         self.key = k
-        self.val = v
+        self.value = v
         self.prev = None
         self.next = None
 
 class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.dic = dict()
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.d = dict()
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def get(self, key):
-        if key in self.dic:
-            n = self.dic[key]
-            self._remove(n)
-            self._add(n)
-            return n.val
+    def get(self, key: int) -> int:
+        if key in self.d:
+            node = self.d[key]
+            self._remove(node)
+            self._append(node)
+            return node.value
         return -1
 
-    def put(self, key, value):
-        if key in self.dic:
-            self._remove(self.dic[key])
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self._remove(self.d[key])
         n = Node(key, value)
-        self._add(n)
-        self.dic[key] = n
-        if len(self.dic) > self.capacity:
-            n = self.head.next
-            self._remove(n)
-            del self.dic[n.key]
-
+        self.d[key] = n
+        self._append(n)
+        if len(self.d) > self.size:
+            last = self.head.next
+            self._remove(last)
+            del self.d[last.key]
+    
+    def _append(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
+    
     def _remove(self, node):
         p = node.prev
         n = node.next
         p.next = n
         n.prev = p
 
-    def _add(self, node):
-        p = self.tail.prev
-        p.next = node
-        self.tail.prev = node
-        node.prev = p
-        node.next = self.tail
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)

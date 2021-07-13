5b5abbdd2294f15a7112a42ae9f1b1fd326e40f1
Sui Yang <suiyang03@gmail.com>
2021-07-13 12:55:34 +0800

    Time: 1944 ms (5.01%), Space: 28.3 MB (8.00%) - LeetHub

--- maximum-gap/maximum-gap.py
+++ maximum-gap/maximum-gap.py
@@ -2,19 +2,19 @@ class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         mi, ma, n = min(nums), max(nums), len(nums)
         if mi == ma: return 0
-        bucketSize = math.ceil((ma - mi) / (n - 1))
-        minBucket = [float('inf')] * n
-        maxBucket = [float('-inf')] * n
+        bucket_size = math.ceil((ma - mi) / (n - 1))
+        min_buckets = [float('inf')] * n
+        max_buckets = [float('-inf')] * n
         
-        for num in nums:
-            idx = (num - mi) // bucketSize
-            minBucket[idx] = min(num, minBucket[idx])
-            maxBucket[idx] = max(num, maxBucket[idx])
+        for n in nums:
+            idx = (n - mi) // bucket_size
+            min_buckets[idx] = min(min_buckets[idx], n)
+            max_buckets[idx] = max(max_buckets[idx], n)
         
-        maxGap = float('-inf')
+        max_gap = float('-inf')
         prev = mi
         for i in range(len(nums)):
-            if minBucket[i] == float('inf'): continue
-            maxGap = max(maxGap, minBucket[i] - prev)
-            prev = maxBucket[i]
-        return maxGap
+            if min_buckets[i] == float('inf'): continue
+            max_gap = max(max_gap, min_buckets[i] - prev)
+            prev = max_buckets[i]
+        return max_gap

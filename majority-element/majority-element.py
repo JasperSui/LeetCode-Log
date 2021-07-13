07c7b0576b8a06a3ae0ff14f4e9c90e28cce2fc1
Sui Yang <suiyang03@gmail.com>
2021-07-13 21:14:25 +0800

    Time: 172 ms (48.06%), Space: 15.5 MB (80.82%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,7 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         d = defaultdict(int)
-        for n in nums:
-            d[n] += 1
-            if d[n] > len(nums) // 2: return n
-        return max(d.items(), key=lambda x: x[1])[0]
+        n = len(nums) // 2
+        for num in nums:
+            d[num] += 1
+            if d[num] > n: return num

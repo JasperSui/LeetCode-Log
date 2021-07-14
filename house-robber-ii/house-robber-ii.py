5e30f696c37a39fefd98113fa441b165b991d1a8
Sui Yang <suiyang03@gmail.com>
2021-07-14 22:37:06 +0800

    Time: 28 ms (87.98%), Space: 14.1 MB (93.29%) - LeetHub

--- house-robber-ii/house-robber-ii.py
+++ house-robber-ii/house-robber-ii.py
@@ -1,13 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
-        left, right = self.rob_sub(nums[:len(nums)-1]), self.rob_sub(nums[1:])
-        return max(left, right)
-        
-        
-    def rob_sub(self, nums):
-        pre2 = pre1 = 0
-        for i in range(len(nums)):
-            curr = max(pre2 + nums[i], pre1)
-            pre2, pre1 = pre1, curr
-        return max(pre2, pre1)
+        def rob_sub(l, r):
+            pre2, pre1 = 0, 0
+            for p in nums[l:r]:
+                curr = max(pre2 + p, pre1)
+                pre2, pre1 = pre1, curr
+            return max(pre2, pre1)
+        return max(rob_sub(0, len(nums)-1), rob_sub(1, len(nums)))

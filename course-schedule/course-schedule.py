1fd12c3f8c81b4a60b0c44921093f4c6418a1812
Sui Yang <suiyang03@gmail.com>
2021-07-14 12:56:29 +0800

    Time: 92 ms (91.10%), Space: 16.5 MB (52.47%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -2,6 +2,7 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = [[] for _ in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
+        
         for x, y in prerequisites:
             graph[x].append(y)
         
@@ -11,18 +12,9 @@ class Solution:
         return True
     
     def dfs(self, graph, visited, i):
-        # -1: being visited, 0: not visited before, 1: have been visited
+        if visited[i] == -1: return False
+        if visited[i] == 1: return True
         
-        # You are visiting a node which is "being visited", so there is a CYCLE HERE
-        # Remember topological sort is must be a DAG (Direct Acyclic Graph)
-        if visited[i] == -1:
-            return False
-        
-        # if the node is visited already, then pass it.
-        if visited[i] == 1:
-            return True
-        
-        # Because you are ready to visit other nodes, mark the current node as -1 for being visited.
         visited[i] = -1
         for j in graph[i]:
             if not self.dfs(graph, visited, j):
@@ -30,3 +22,65 @@ class Solution:
         
         visited[i] = 1
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         graph = [[] for _ in range(numCourses)]
+#         visited = [0 for _ in range(numCourses)]
+#         for x, y in prerequisites:
+#             graph[x].append(y)
+#         print(graph)
+        
+#         for i in range(numCourses):
+#             if not self.dfs(graph, visited, i):
+#                 return False
+#         return True
+    
+#     def dfs(self, graph, visited, i):
+#         # -1: being visited, 0: not visited before, 1: have been visited
+        
+#         # You are visiting a node which is "being visited", so there is a CYCLE HERE
+#         # Remember topological sort is must be a DAG (Direct Acyclic Graph)
+#         if visited[i] == -1:
+#             return False
+        
+#         # if the node is visited already, then pass it.
+#         if visited[i] == 1:
+#             return True
+        
+#         # Because you are ready to visit other nodes, mark the current node as -1 for being visited.
+#         visited[i] = -1
+#         for j in graph[i]:
+#             if not self.dfs(graph, visited, j):
+#                 return False
+        
+#         visited[i] = 1
+#         return True

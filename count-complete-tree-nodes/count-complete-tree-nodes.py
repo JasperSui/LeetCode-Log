a4a6c806d07bb9c5c83f194960963239115377d8
Sui Yang <suiyang03@gmail.com>
2021-07-14 23:08:53 +0800

    Time: 72 ms (83.50%), Space: 21.5 MB (55.16%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.py
+++ count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -7,14 +7,14 @@
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
         if not root: return 0
-        def get_depth(node):
+        def depth(node):
             if not node: return 0
-            return 1 + get_depth(node.left)
+            return 1 + depth(node.left)
         
-        left_depth = get_depth(root.left)
-        right_depth = get_depth(root.right)
+        left_depth = depth(root.left)
+        right_depth = depth(root.right)
         
-        if left_depth == right_depth:
+        if left_depth == right_depth: # left tree is perfect binary tree
             return pow(2, left_depth) + self.countNodes(root.right)
         else:
             return pow(2, right_depth) + self.countNodes(root.left)

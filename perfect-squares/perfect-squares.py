716daf3c204a9b2aacccaabbe177a83ec5a43c6d
Sui Yang <suiyang03@gmail.com>
2021-07-15 21:16:54 +0800

    Time: 176 ms (87.48%), Space: 15.1 MB (26.35%) - LeetHub

--- perfect-squares/perfect-squares.py
+++ perfect-squares/perfect-squares.py
@@ -1,5 +1,68 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        if n <= 2: return n
+        squares = []
+        i = 1
+        while i*i <= n:
+            squares.append(i*i)
+            i += 1
+        
+        ans = 0
+        stack = {n}
+        while stack:
+            temp = set()
+            ans += 1
+            for i in stack:
+                for square in squares:
+                    if square == i:
+                        return ans
+                    if i < square:
+                        break
+                    temp.add(i-square)
+            stack = temp
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         if n < 2: return n
         squares = []
         i = 1

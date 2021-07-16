97f1361691f185d25e0a672a5175e1e3a382e8f2
Sui Yang <suiyang03@gmail.com>
2021-07-16 22:05:17 +0800

    Time: 76 ms (91.27%), Space: 20.7 MB (90.86%) - LeetHub

--- counting-bits/counting-bits.py
+++ counting-bits/counting-bits.py
@@ -6,7 +6,15 @@ class Solution:
         #     res += [i+1 for i in res]
         # return res[:num+1]
         
-        res = [0] * (num + 1) 
+        # res = [0] * (num + 1) 
+        # for i in range(1, num+1):
+        #     res[i] = res[i//2] + i % 2
+        # return res
+        
+        res = [0] * (num+1)
+        offset = 1
         for i in range(1, num+1):
-            res[i] = res[i//2] + i % 2
+            if offset * 2 == i: offset *= 2
+            res[i] = res[i - offset] + 1
         return res
+        

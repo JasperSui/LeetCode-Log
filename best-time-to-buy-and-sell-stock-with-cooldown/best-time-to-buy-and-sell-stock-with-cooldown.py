74c2902ce11e4dfe2c358cd0ec0756fa9b13741d
Sui Yang <suiyang03@gmail.com>
2021-07-16 13:19:40 +0800

    Time: 40 ms (73.43%), Space: 14.5 MB (82.80%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp_i_0, dp_i_1 = 0, float('-inf')
-        dp_pre_0 = 0
-        for i in range(len(prices)):
+        dp_i_0, dp_i_1, dp_pre_0 = 0, float('-inf'), 0
+        for p in prices:
             temp = dp_i_0
-            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-            dp_i_1 = max(dp_i_1, dp_pre_0 - prices[i])
+            dp_i_0 = max(dp_i_0, dp_i_1 + p)
+            dp_i_1 = max(dp_i_1, dp_pre_0 - p)
             dp_pre_0 = temp
         return dp_i_0

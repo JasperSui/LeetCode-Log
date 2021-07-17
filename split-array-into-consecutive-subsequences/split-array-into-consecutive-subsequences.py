ff6883f5c2eb49ccc033fa336dce86c90ee9dd33
Sui Yang <suiyang03@gmail.com>
2021-07-18 02:03:12 +0800

    Time: 556 ms (57.08%), Space: 15.6 MB (12.99%) - LeetHub

---
+++ split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        left = collections.Counter(nums)
+        end = defaultdict(int)
+        for n in nums:
+            if not left[n]: continue
+            left[n] -= 1
+            if end[n-1] > 0:
+                end[n-1] -= 1
+                end[n] += 1
+            elif left[n+1] and left[n+2]:
+                left[n+1] -= 1
+                left[n+2] -= 1
+                end[n+2] += 1
+            else:
+                return False
+        return True

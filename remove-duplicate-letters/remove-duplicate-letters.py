cc55c0995f736e3bd509913b9f0ffb4e70d37a3c
Sui Yang <suiyang03@gmail.com>
2021-07-18 00:13:46 +0800

    Time: 116 ms (5.14%), Space: 14.6 MB (21.53%) - LeetHub

---
+++ remove-duplicate-letters/remove-duplicate-letters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        if not s: return ""
+        counts = collections.Counter(s)
+        pos = 0
+        for i, c in enumerate(s):
+            if c < s[pos]:
+                pos = i
+            counts[c] -= 1
+            if counts[c] == 0:
+                break
+        return s[pos] + self.removeDuplicateLetters(s[pos+1:].replace(s[pos], ""))

a6583b9ab24412094d1990c1d1f1ce9696186af9
Sui Yang <suiyang03@gmail.com>
2021-07-19 21:40:05 +0800

    Time: 160 ms (92.12%), Space: 20 MB (80.16%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -2,8 +2,8 @@ class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         low, high = matrix[0][0], matrix[-1][-1]
         while low < high:
-            mid = (low+high) // 2
-            if sum(bisect.bisect_right(row, mid) for row in matrix) < k:
+            mid = low + (high - low) // 2
+            if sum(bisect.bisect(row, mid) for row in matrix) < k:
                 low = mid + 1
             else:
                 high = mid

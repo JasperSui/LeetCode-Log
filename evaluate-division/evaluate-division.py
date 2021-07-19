c9a89a92b8ba0a1d55f5f3826438869bb0046a3f
Sui Yang <suiyang03@gmail.com>
2021-07-19 22:14:38 +0800

    Time: 32 ms (68.39%), Space: 14.2 MB (91.77%) - LeetHub

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -1,19 +1,16 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         d = defaultdict(set)
-        for (a, b), v in zip(equations, values):
-            # for example, a/b = 2.0, then b/a will be 0.5
-            # so we need to do these both
-            d[a].add((b, v))
-            d[b].add((a, 1/v))
+        for (x, y), v in zip(equations, values):
+            d[x].add((y, v))
+            d[y].add((x, 1/v))
         
         def find(a, b):
-            if not a in d and not b in d:
+            if a not in d or b not in d:
                 return -1.0
-
             queue = [(a, 1.0)]
             visited = set()
-
+            
             while queue:
                 new_queue = []
                 for front, curr_val in queue:
@@ -22,9 +19,9 @@ class Solution:
                     visited.add(front)
                     for neigh, value in d[front]:
                         if neigh not in visited:
-                            new_queue.append((neigh, curr_val * value))
+                            new_queue.append((neigh, value * curr_val))
                 queue = new_queue
-
             return -1.0
         
         return [find(x, y) for x, y in queries]
+            

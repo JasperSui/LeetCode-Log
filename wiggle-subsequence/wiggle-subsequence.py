88399d402873a477862bcb1262fa7f83d2fec71d
Sui Yang <suiyang03@gmail.com>
2021-07-19 13:05:03 +0800

    Time: 36 ms (64.52%), Space: 14.2 MB (64.41%) - LeetHub

--- wiggle-subsequence/wiggle-subsequence.py
+++ wiggle-subsequence/wiggle-subsequence.py
@@ -4,14 +4,13 @@ class Solution:
         down = [0] * len(nums)
         up[0], down[0] = 1, 1
         for i in range(1, len(nums)):
-            if nums[i-1] < nums[i]:
+            if nums[i] > nums[i-1]:
                 up[i] = down[i-1] + 1
                 down[i] = down[i-1]
-            elif nums[i-1] > nums[i]:
+            elif nums[i] < nums[i-1]:
+                up[i] = up[i-1]
                 down[i] = up[i-1] + 1
+            else:
                 up[i] = up[i-1]
-            elif nums[i-1] == nums[i]:
                 down[i] = down[i-1]
-                up[i] = up[i-1]
         return max(up[-1], down[-1])
-            

9556e55c4534ff4938dc322b66f2c2ab96467f0e
Sui Yang <suiyang03@gmail.com>
2021-07-19 21:56:14 +0800

    Time: 552 ms (13.93%), Space: 65.9 MB (23.74%) - LeetHub

--- insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -5,38 +5,36 @@ class RandomizedCollection:
         Initialize your data structure here.
         """
         self.nums = []
-        self.indexes = defaultdict(set)
-        
+        self.pos = defaultdict(set)
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
         """
         self.nums.append(val)
-        self.indexes[val].add(len(self.nums)-1)
-        return len(self.indexes[val]) == 1
-            
+        self.pos[val].add(len(self.nums)-1)
+        return len(self.pos[val]) == 1
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the collection. Returns true if the collection contained the specified element.
         """
-        if self.indexes[val]:
-            index = self.indexes[val].pop()
+        if self.pos[val]:
+            index = self.pos[val].pop()
             last = self.nums[-1]
             self.nums[index] = last
-            self.indexes[last].add(index)
-            self.indexes[last].remove(len(self.nums) - 1)
+            self.pos[last].add(index)
+            self.pos[last].remove(len(self.nums)-1)
             self.nums.pop()
             return True
         return False
-        
 
     def getRandom(self) -> int:
         """
         Get a random element from the collection.
         """
-        return random.choice(self.nums)
+        return self.nums[random.randint(0, len(self.nums)-1)]
+        
 
 
 # Your RandomizedCollection object will be instantiated and called as such:

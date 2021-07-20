9a820b63647b49830c80f91ede908373f319aa26
Sui Yang <suiyang03@gmail.com>
2021-07-20 13:33:50 +0800

    Time: 76 ms (88.82%), Space: 14.7 MB (49.34%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.py
+++ longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,47 +1,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0] * (len(nums) + 1)
-        for i in range(len(nums)):
-            m = 1
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    m = max(m, dp[j] + 1)
-            dp[i] = m
-        return max(dp)
-         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # tails = [0] * len(nums)
-        # size = 0
-        # for n in nums:
-        #     low, high = 0, size
-        #     while low < high:
-        #         mid = low + (high - low) // 2
-        #         if tails[mid] < n:
-        #             low = mid + 1
-        #         else:
-        #             high = mid
-        #     tails[low] = n
-        #     size = max(size, low+1)
-        # return size
-        
-#         dp = [0] * (len(nums)+1)
-#         for i in range(len(nums)):
-#             m = 1
-#             for j in range(i):
-#                 if nums[i] > nums[j]:
-#                     m = max(m, dp[j] + 1)
+        tails = [0] * len(nums)
+        size = 0
+        for n in nums:
+            low, high = 0, size
+            while low != high:
+                mid = low + (high - low) // 2
+                if tails[mid] < n:
+                    low = mid + 1
+                else:
+                    high = mid
+            tails[low] = n
+            size = max(size, low + 1)
+        return size
             
-#             dp[i] = m
-#         return max(dp)

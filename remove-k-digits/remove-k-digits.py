8625f0f5692a43a74089d714ae1214ff3a3c5d05
Sui Yang <suiyang03@gmail.com>
2021-07-20 12:59:36 +0800

    Time: 32 ms (95.61%), Space: 14.6 MB (35.23%) - LeetHub

--- remove-k-digits/remove-k-digits.py
+++ remove-k-digits/remove-k-digits.py
@@ -3,10 +3,10 @@ class Solution:
         stack = []
         for n in num:
             while k > 0 and stack and stack[-1] > n:
-                k -= 1
                 stack.pop()
+                k -= 1
             stack.append(n)
-        
         if k > 0:
             stack = stack[:-k]
         return "".join(stack).lstrip("0") or "0"
+            

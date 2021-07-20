4f00a8330599572fcabacaa8206687fac3f957fc
Sui Yang <suiyang03@gmail.com>
2021-07-20 13:01:46 +0800

    Time: 36 ms (48.15%), Space: 15.3 MB (9.75%) - LeetHub

--- sum-of-left-leaves/sum-of-left-leaves.py
+++ sum-of-left-leaves/sum-of-left-leaves.py
@@ -7,10 +7,11 @@
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
         self.ans = 0
-        def dfs(node, is_left=True):
+        def dfs(node, calc=False):
             if not node: return
-            dfs(node.left)
-            if is_left and not node.left and not node.right: self.ans += node.val
-            dfs(node.right, False)
-        dfs(root, False)
+            if calc and not node.left and not node.right:
+                self.ans += node.val
+            dfs(node.left, True)
+            dfs(node.right)
+        dfs(root)
         return self.ans

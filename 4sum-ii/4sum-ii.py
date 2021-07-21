3196aae5f34fdb38427f2d7fc1ea1278d8be9ab2
Sui Yang <suiyang03@gmail.com>
2021-07-22 01:30:23 +0800

    Time: 588 ms (93.85%), Space: 14.5 MB (45.01%) - LeetHub

--- 4sum-ii/4sum-ii.py
+++ 4sum-ii/4sum-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        dic = {}
+        dic = defaultdict(int)
         ans = 0
+
         for a in nums1:
             for b in nums2:
-                dic[a+b] = dic.get(a+b, 0) + 1
+                dic[a+b] += 1
         
         for c in nums3:
             for d in nums4:
-                ans += dic.get(-c-d, 0)
+                ans += dic[-c-d]
         return ans

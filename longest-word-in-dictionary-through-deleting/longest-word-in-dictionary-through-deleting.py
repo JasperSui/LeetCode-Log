6e55f12da5d2e258127b452369c8190a3903d242
Sui Yang <suiyang03@gmail.com>
2021-07-22 13:07:21 +0800

    Time: 280 ms (73.51%), Space: 16.8 MB (61.27%) - LeetHub

--- longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
+++ longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
@@ -1,6 +1,19 @@
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        def isSubSequence(x):
-            it = iter(s)
-            return all(c in it for c in x)
-        return min(list(filter(isSubSequence, dictionary)) + [''], key= lambda x: (-len(x), x))
+        res = ""
+        for word in dictionary:
+            
+            data = self.validate(s, word)
+            if len(data) > len(res):
+                res = data
+            elif len(data) == len(res):
+                res = min(res, data)
+        return res
+        
+    def validate(self, s, word):
+        stack = list(word[::-1])
+        for c in s:
+            if not stack: break
+            if stack[-1] == c:
+                stack.pop()
+        return "" if stack else word

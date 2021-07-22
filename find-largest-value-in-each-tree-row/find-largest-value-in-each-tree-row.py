61c2cdd4c3f983f6ecdf7ef138892082d8e2fab9
Sui Yang <suiyang03@gmail.com>
2021-07-22 13:10:55 +0800

    Time: 40 ms (95.58%), Space: 16.5 MB (59.20%) - LeetHub

--- find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: TreeNode) -> List[int]:
-        
-        # BFS
         res = []
-        levels = [root]
-        while any(levels):
-            res.append(max(node.val for node in levels))
-            levels = [kid for node in levels for kid in (node.left, node.right) if kid]
+        stack = [root]
+        while stack:
+            new_stack = []
+            curr_max = float('-inf')
+            for node in stack:
+                if node:
+                    curr_max = max(curr_max, node.val)
+                    new_stack.append(node.left)
+                    new_stack.append(node.right)
+            if curr_max != float('-inf'):
+                res.append(curr_max)
+            stack = new_stack
         return res
-            
-            
-        # DFS (a little stupid)
-        # if not root: return []
-        # self.level = 0
-        # self.d = defaultdict(lambda: float('-inf'))
-        # def dfs(node, level=1):
-        #     if not node: return
-        #     dfs(node.left, level+1)
-        #     self.d[level] = max(self.d[level], node.val)
-        #     dfs(node.right, level+1)
-        #     self.level = level+1
-        # dfs(root)
-        # max_key = max(self.d.keys())
-        # return [self.d[i] for i in range(1, max_key+1)]

80462a7f26197e801b127d14290485bd638a6828
Sui Yang <suiyang03@gmail.com>
2021-07-22 21:24:59 +0800

    Time: 820 ms (97.50%), Space: 18.9 MB (56.08%) - LeetHub

--- contiguous-array/contiguous-array.py
+++ contiguous-array/contiguous-array.py
@@ -1,15 +1,13 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        count, max_len = 0, 0
         d = {0: 0}
-        ans, count = 0, 0
         for index, num in enumerate(nums, 1):
-            if num == 0:
-                count -= 1
-            else:
-                count += 1
+            if num == 0: count -= 1
+            else: count += 1
             
             if count in d:
-                ans = max(ans, index - d[count])
+                max_len = max(max_len, index - d[count])
             else:
                 d[count] = index
-        return ans
+        return max_len

0acd479f2eb0938f8668bfa60044d50900e37d20
Sui Yang <suiyang03@gmail.com>
2021-07-23 22:51:17 +0800

    Time: 192 ms (16.01%), Space: 15.4 MB (17.79%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.py
+++ subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,39 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        self.res = False
         def check(n1, n2):
             if not n1 and not n2: return True
             if not n1 or not n2: return False
             if n1.val != n2.val: return False
             return check(n1.left, n2.left) and check(n1.right, n2.right)
-        
-        def dfs(n1, n2):
-            if not n1: return False
-            if n1.val == n2.val and check(n1, n2): return True
-            return dfs(n1.left, t) or dfs(n1.right, t)
-        return dfs(s, t)
-                
-                
-                
-        
-        
-#         # Time: O(S*T)
-#         # Space: O(height)
-        
-#         # Solution 1 (Recursive)
-#         if not t: return True
-#         def check(n1, n2):
-#             if not n1 and not n2: return True
-#             if not n1 or not n2: return False
-#             if n1.val != n2.val: return False
-#             return check(n1.left, n2.left) and check(n1.right, n2.right)
-        
-#         def dfs(n1, n2):
-#             if not n1: return False
-#             if n1.val == n2.val and check(n1, n2): return True
-#             return dfs(n1.left, t) or dfs(n1.right, t)
-        
-#         return dfs(s, t)
-        
+
+        def dfs(node):
+            if not node: return
+            if node.val == subRoot.val:
+                if check(node, subRoot): self.res = True
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return self.res
+            

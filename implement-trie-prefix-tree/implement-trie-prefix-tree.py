5d6c8b3e1f20213af863e59f901d55b26de4add5
Sui Yang <suiyang03@gmail.com>
2021-07-24 11:03:59 +0800

    Time: 120 ms (98.11%), Space: 27.7 MB (91.41%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,51 +1,43 @@
-class TrieNode:
-    def __init__(self):
-        self.links = [None] * 26
-        self.is_finished = False
 class Trie:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.root = TrieNode()
+        self.trie = {}
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        curr = self.root
+        node = self.trie
         for c in word:
-            index = ord(c) - ord('a')
-            if not curr.links[index]:
-                curr.links[index] = TrieNode()
-            curr = curr.links[index]
-        curr.is_finished = True
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node['#'] = '#'
         
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        curr = self.root
+        node = self.trie
         for c in word:
-            index = ord(c) - ord('a')
-            if not curr.links[index]: return False
-            curr = curr.links[index]
-        return curr.is_finished
+            if c not in node: return False
+            node = node[c]
+        return "#" in node
         
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        curr = self.root
+        node = self.trie
         for c in prefix:
-            index = ord(c) - ord('a')
-            if not curr.links[index]: return False
-            curr = curr.links[index]
+            if c not in node: return False
+            node = node[c]
         return True
-        
 
 
 # Your Trie object will be instantiated and called as such:

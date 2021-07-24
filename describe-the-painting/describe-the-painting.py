c2ff223a21792ded6c51b0a6901014afbd10a260
Sui Yang <suiyang03@gmail.com>
2021-07-25 01:30:16 +0800

    Time: 1224 ms (100.00%), Space: 33.7 MB (100.00%) - LeetHub

---
+++ describe-the-painting/describe-the-painting.py
@@ -0,0 +1,14 @@
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
+        d = defaultdict(int)
+        for i, j, c in segments:
+            d[i] += c
+            d[j] -= c
+        res = []
+        i = 0
+        for j in sorted(d):
+            if d[i]:
+                res.append([i, j, d[i]])
+            d[j] += d[i]
+            i = j
+        return res

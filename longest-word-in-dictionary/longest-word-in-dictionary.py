b193b159add5fa271b0ff95d580cb734d48a1503
Sui Yang <suiyang03@gmail.com>
2021-07-25 23:09:57 +0800

    Time: 144 ms (44.52%), Space: 15.2 MB (28.42%) - LeetHub

---
+++ longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.word = ""
+        self.is_word = False
+        self.links = defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            node = node.links[c]
+        node.is_word = True
+        node.word = word
+    
+    def bfs(self):
+        queue = deque([self.root])
+        res = ""
+        while queue:
+            curr = queue.popleft()
+            for node in curr.links.values():
+                if node.is_word:
+                    queue.append(node)
+                    if len(node.word) > len(res) or node.word < res:
+                        res = node.word
+        return res
+        
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        t = Trie()
+        for word in words:
+            t.insert(word)
+        return t.bfs()

ed8880d911c44cac536d88e3d3bd1da854ba7300
Sui Yang <suiyang03@gmail.com>
2021-07-25 23:27:24 +0800

    Time: 492 ms (78.21%), Space: 15.6 MB (77.40%) - LeetHub

---
+++ number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        word_dict = defaultdict(list)
+        ans = 0
+        
+        for word in words:
+            word_dict[word[0]].append(word)
+        
+        for c in s:
+            word_list = word_dict[c]
+            word_dict[c] = []
+            for word in word_list:
+                if len(word) == 1: ans += 1
+                else:
+                    word_dict[word[1]].append(word[1:])
+        return ans

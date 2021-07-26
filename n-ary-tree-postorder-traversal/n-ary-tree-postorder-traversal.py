919912152bdd7fb93c8476a7420215926db9057d
Sui Yang <suiyang03@gmail.com>
2021-07-26 12:32:40 +0800

    Time: 40 ms (98.76%), Space: 16.2 MB (26.29%) - LeetHub

--- n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -11,43 +11,8 @@ class Solution:
         self.res = []
         def dfs(node):
             if not node: return
-            for c in node.children:
-                dfs(c)
+            for child in node.children:
+                dfs(child)
             self.res.append(node.val)
         dfs(root)
         return self.res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # Time: O(N)
-        # Space: O(height)
-        
-        # Iterable (using stack)
-        res = []
-        if not root: return res
-        stack = [root]
-        while stack:
-            curr = stack.pop()
-            res.append(curr.val)
-            stack.extend(curr.children)
-        return res[::-1] # Post Order
-        
-        # # Recursive
-        # res = []
-        # def dfs(node):
-        #     if not node: return
-        #     if node.children:
-        #         for c in node.children:
-        #             dfs(c)
-        #     res.append(node.val)
-        # dfs(root)
-        # return res

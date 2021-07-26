5bab5d8b814a369179287831bfd5e10e9b7d869a
Sui Yang <suiyang03@gmail.com>
2021-07-26 13:28:56 +0800

    Time: 452 ms (44.31%), Space: 15 MB (25.26%) - LeetHub

--- task-scheduler/task-scheduler.py
+++ task-scheduler/task-scheduler.py
@@ -4,16 +4,17 @@ class Solution:
         ma = 0
         max_count = 0
         for c in tasks:
-            d[ord(c) - ord("A")] += 1
-            if d[ord(c) - ord("A")] > ma:
-                ma = d[ord(c) - ord("A")]
+            index = ord(c) - ord('A')
+            d[index] += 1
+            if d[index] > ma:
+                ma = d[index]
                 max_count = 1
-            elif d[ord(c) - ord("A")] == ma:
+            elif d[index] == ma:
                 max_count += 1
         
         part_count = ma - 1
         part_len = n - (max_count - 1)
         empty_slots = part_count * part_len
-        available_tasks = len(tasks) - ma * max_count
+        available_tasks = len(tasks) - (ma * max_count)
         idles = max(0, empty_slots - available_tasks)
         return len(tasks) + idles

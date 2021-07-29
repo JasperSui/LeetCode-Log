a159bfd0ec8d8b077f83ca0e4ed6be27c6324a15
Sui Yang <suiyang03@gmail.com>
2021-07-29 13:01:13 +0800

    Time: 32 ms (79.72%), Space: 14.1 MB (84.05%) - LeetHub

--- reorganize-string/reorganize-string.py
+++ reorganize-string/reorganize-string.py
@@ -1,40 +1,16 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # Max heap
-        res, d = [], collections.Counter(s)
-        pq = [(-times, char) for char, times in d.items()] # Max heap priority queue
+        d = Counter(s)
+        res = []
+        pq = [(-times, key) for key, times in d.items()]
         heapq.heapify(pq)
         prev_a, prev_b = 0, ''
         while pq:
             a, b = heapq.heappop(pq)
-            res.append(b)
             if prev_a < 0:
                 heapq.heappush(pq, (prev_a, prev_b))
-            a += 1 # means -1 for times
+            a += 1
+            res.append(b)
             prev_a, prev_b = a, b
-        if len(res) != len(s): return ""
-        return "".join(res)
-        
-        
-        
-        # res = []
-        # d = collections.Counter(s)
-        # keys = set(d.keys())
-        # last = None
-        # while len(res) != len(s):
-        #     c, _ = max(d.items(), key=lambda x: x[1])
-        #     if last != c:
-        #         res.append(c)
-        #         d[c] -= 1
-        #         last = c
-        #     else:
-        #         max_char, max_count = "", 0
-        #         for char in keys - set(c):
-        #             if d[char] > max_count:
-        #                 max_count = d[char]
-        #                 max_char = char
-        #         if max_count == 0: return ""
-        #         res.append(max_char)
-        #         d[max_char] -= 1
-        #         last = max_char
-        # return "".join(res)
+        if len(res) == len(s): return "".join(res)
+        else: return ""

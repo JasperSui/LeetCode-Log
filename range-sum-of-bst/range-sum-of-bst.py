dc2aad0c3ba24c5ec50552e47517fc855107d182
Sui Yang <suiyang03@gmail.com>
2021-08-02 13:35:48 +0800

    Time: 216 ms (60.64%), Space: 22.3 MB (18.21%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.py
+++ range-sum-of-bst/range-sum-of-bst.py
@@ -9,49 +9,11 @@ class Solution:
         self.ans = 0
         def dfs(node):
             if not node: return
-            if node.val > R:
-                dfs(node.left)
-            elif node.val < L:
-                dfs(node.right)
-            else:
+            if node.val < L: dfs(node.right)
+            if node.val > R: dfs(node.left)
+            if L <= node.val <= R:
                 dfs(node.left)
                 dfs(node.right)
-                
-            if L <= node.val <= R: self.ans += node.val
+                self.ans += node.val
         dfs(root)
         return self.ans
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # self.ans = 0
-        # def calc(root, L, R):
-        #     if not root:
-        #         return None
-        #     if root.val and L <= root.val <= R:
-        #         self.ans += root.val
-        #     calc(root.left, L, R)
-        #     calc(root.right, L, R)
-        # calc(root, L, R)
-        # return self.ans
-        if not root:
-            return 0
-        return self.rangeSumBST(root.left, L, R) + \
-               self.rangeSumBST(root.right, L, R) + \
-               (root.val if L <= root.val <= R else 0)

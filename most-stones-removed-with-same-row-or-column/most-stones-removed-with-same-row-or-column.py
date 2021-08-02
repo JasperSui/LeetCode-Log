d8e8928e975f27c8ebf4a8e438372c674f60435d
Sui Yang <suiyang03@gmail.com>
2021-08-02 13:40:45 +0800

    Time: 3572 ms (5.04%), Space: 14.9 MB (79.69%) - LeetHub

--- most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,28 +1,24 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
+        parent_count = 0
+        n = len(stones)
+        parent = list(range(1000))
+        
         def union(x, y):
-            x = find(x)
-            y = find(y)
-            if x == y: return
-            parent[y] = x
+            parent[find(y)] = find(x)
         
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
         
-        n = len(stones)
-        parent = list(range(1000))
-        
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(1, n):
                 if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
                     union(i, j)
         
-        ans = 0
         for i in range(n):
             if parent[i] == i:
-                ans += 1
-        return n - ans
+                parent_count += 1
         
-        
+        return n - parent_count

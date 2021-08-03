5e7be31356dfbe20dc47c2a78b95000279e58e38
Sui Yang <suiyang03@gmail.com>
2021-08-03 13:06:59 +0800

    Time: 36 ms (58.79%), Space: 14.6 MB (63.22%) - LeetHub

--- vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,75 +6,20 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
-        self.temp = []
-        def dfs(node, x=0, y=0):
+        self.d = defaultdict(lambda: defaultdict(list))
+        def dfs(node, x=0, depth=0):
             if not node: return
-            self.temp.append((node.val, x, y))
-            dfs(node.left, x-1, y+1)
-            dfs(node.right, x+1, y+1)
+            self.d[x][depth].append(node.val)
+            dfs(node.left, x-1, depth+1)
+            dfs(node.right, x+1, depth+1)
         dfs(root)
-        self.temp.sort(key=lambda x: (x[1], x[2], x[0]))
-        res = defaultdict(list)
-        for v, x, _ in self.temp:
-            res[x].append(v)
-        return res.values()
-        
-        
-        # Time: O(NlogN)
-        # Sapce: O(N)
-        
-        # # Iterative
-        # x = y = 0
-        # res = defaultdict(lambda: defaultdict(list))
-        # queue = [(root, 0, 0)]
-        # while queue:
-        #     node, x, y = queue.pop(0)
-        #     if node:
-        #         res[x][y].append(node.val)
-        #         queue.append((node.left, x-1, y+1))
-        #         queue.append((node.right, x+1, y+1))
-        # res = dict(sorted(res.items()))
-        # res_2 = []
-        # for x, d in res.items():
-        #     tmp = []
-        #     for y in d.keys():
-        #         res[x][y].sort()
-        #         tmp.extend(res[x][y])
-        #     res_2.append(tmp)
-        # return res_2
-        
-        # Recursive
-        vals = []
-        def preorder(node, x=0, y=0):
-            if not node: return
-            vals.append((x, y, node.val))
-            preorder(node.left, x-1, y+1)
-            preorder(node.right, x+1, y+1)
-        preorder(root)
-        ans = []
-        last_x = -1000
-        for x, y, v in sorted(vals):
-            if x != last_x:
-                ans.append([])
-                last_x = x
-            ans[-1].append(v)
-        return ans
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        self.d = dict(sorted(self.d.items()))
+        res = []
+        for d in self.d.values():
+            d = dict(sorted(d.items()))
+            temp = []
+            for l in d.values():
+                l.sort()
+                temp.extend([*l])
+            res.append(temp)
+        return res

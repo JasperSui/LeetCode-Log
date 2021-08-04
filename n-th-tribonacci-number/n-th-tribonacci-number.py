f9ebf1b1f1187880c89d94ad4c6c14482e9de5bd
Sui Yang <suiyang03@gmail.com>
2021-08-04 13:09:03 +0800

    Time: 24 ms (94.69%), Space: 14.3 MB (44.67%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.py
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def tribonacci(self, n: int, dp={}) -> int:
         if n == 0: return 0
-        elif n == 1: return 1
-        elif n == 2: return 1
+        elif n == 1 or n == 2: return 1
         if n in dp: return dp[n]
-        dp[n] = self.tribonacci(n-1, dp) + self.tribonacci(n-2, dp) + self.tribonacci(n-3, dp)
+        dp[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
         return dp[n]

89d6dfc06b67948af67f12778151e4362de607eb
Sui Yang <suiyang03@gmail.com>
2021-08-05 13:07:23 +0800

    Time: 32 ms (64.38%), Space: 14.3 MB (38.11%) - LeetHub

--- convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
+++ convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
@@ -5,16 +5,8 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        # res = ""
-        # while head:
-        #     res += str(head.val)
-        #     head = head.next
-        # return int(res, 2)
-        
-        ans = 0
+        ans = ""
         while head:
-            # 等於往左推一個bit，高位拋棄，低位補0，最後 | 就是再加上10進位制的
-            # 4 (100) << 1 就變成 8 (1000), 6 (110) << 1 就變成 12 (1100)
-            ans = (ans << 1) | head.val
+            ans += str(head.val)
             head = head.next
-        return ans
+        return int(ans, 2)

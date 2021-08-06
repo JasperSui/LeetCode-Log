de357562aa28e7b195f97e13af3449f276457b07
Sui Yang <suiyang03@gmail.com>
2021-08-06 12:54:12 +0800

    Time: 32 ms (54.58%), Space: 13.9 MB (98.24%) - LeetHub

--- maximum-69-number/maximum-69-number.py
+++ maximum-69-number/maximum-69-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        ans = num
-        s = str(num)
-        n = len(s)
-        for i in range(n):
-            if s[i] == '9': continue
-            else:
-                ans = max(ans, num + 3 * (10 ** (n - 1 - i)))
-        return ans
+        for i, n in enumerate(str(num)):
+            if n == '6':
+                return num + 3 * 10 ** (len(str(num)) - i - 1)
+        return num

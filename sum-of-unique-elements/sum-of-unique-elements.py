3c17a358cf735cb1b2d4641617b023b58614f758
Sui Yang <suiyang03@gmail.com>
2021-08-11 12:48:49 +0800

    Time: 36 ms (50.51%), Space: 14.3 MB (11.22%) - LeetHub

--- sum-of-unique-elements/sum-of-unique-elements.py
+++ sum-of-unique-elements/sum-of-unique-elements.py
@@ -1,13 +1,6 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        seen, subtracted = set(), set()
-        ans = 0
+        d = defaultdict(int)
         for n in nums:
-            if n in seen:
-                if n not in subtracted:
-                    ans -= n
-                    subtracted.add(n)
-            else:
-                ans += n
-                seen.add(n)
-        return ans
+            d[n] += 1
+        return sum(k for k, v in d.items() if v == 1)

892925b578d79d64add5e309a1202862791400fe
Sui Yang <suiyang03@gmail.com>
2021-08-17 13:11:02 +0800

    Time: 28 ms (84.44%), Space: 14.3 MB (32.96%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,21 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         mapping = {
-            '1': '',
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-            '0': ' '}
-        res = [''] if digits else []
+            "1": "",
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        res = ['']
+        if not digits: return []
         for d in digits:
             temp = []
-            for q in mapping[d]:
-                for p in res:
+            for p in res:
+                for q in mapping[d]:
                     temp.append(p+q)
             res = temp
         return res

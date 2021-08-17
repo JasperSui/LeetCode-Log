6c26fa29fb3391c74eb38d20a2ac149c07e069e4
Sui Yang <suiyang03@gmail.com>
2021-08-17 12:58:31 +0800

    Time: 904 ms (63.68%), Space: 17.6 MB (50.16%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -6,16 +6,18 @@ class Solution:
             if i > 0 and nums[i] == nums[i-1]: continue
             front, end = i+1, len(nums)-1
             while front < end:
-                s = nums[i] + nums[front] + nums[end]
-                if s > 0:
-                    end -= 1
-                elif s < 0:
-                    front += 1
-                elif s == 0:
-                    res.append([nums[i], nums[front], nums[end]])
-
-                    while front < end and nums[front] == nums[front+1]: front += 1
-                    while front < end and nums[end] == nums[end-1]: end -= 1
+                s = nums[front] + nums[i] + nums[end]
+                if s == 0:
+                    res.append([nums[front], nums[i], nums[end]])
+                    while front < end and nums[front] == nums[front+1]:
+                        front += 1
+                    while front < end and nums[end] == nums[end-1]:
+                        end -= 1
+                    
                     front += 1
                     end -= 1
+                elif s > 0:
+                    end -= 1
+                else:
+                    front += 1
         return res

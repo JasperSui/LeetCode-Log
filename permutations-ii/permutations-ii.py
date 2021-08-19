35bd93b06e56c90f340fc61ad165d08ade473207
Sui Yang <suiyang03@gmail.com>
2021-08-19 13:16:05 +0800

    Time: 56 ms (85.34%), Space: 14.4 MB (79.31%) - LeetHub

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -1,33 +1,22 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        self.res = [nums]
-        temp = nums.copy()
-        while 1:
-            temp = self.get_next_permutation(temp)
-            if temp and temp not in self.res:
-                self.res.append(temp.copy())
-            else:
-                break
-        return self.res
+        nums.sort()
+        res = []
+        used = [False for _ in range(len(nums))]
+        self.dfs(nums, [], used, res)
+        return res
     
-    def get_next_permutation(self, nums):
-        i = j = len(nums)-1
-        while i > 0 and nums[i] <= nums[i-1]:
-            i -= 1
+    def dfs(self, nums, path, used, res):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
         
-        if i == 0:
-            nums.reverse()
-            return nums
-        k = i - 1
+        for i in range(len(nums)):
+            if used[i]: continue
+            if i > 0 and nums[i-1] == nums[i] and not used[i-1]: continue
+            used[i] = True
+            path.append(nums[i])
+            self.dfs(nums, path, used, res)
+            used[i] = False
+            path.pop()
         
-        while j > 0 and nums[j] <= nums[k]:
-            j -= 1
-        
-        nums[j], nums[k] = nums[k], nums[j]
-        
-        low, high = k+1, len(nums)-1
-        while low < high:
-            nums[low], nums[high] = nums[high], nums[low]
-            low += 1
-            high -= 1
-        return nums

32322b6ae684ab6ed6f40a7a45ee9a28b0a36c9a
Sui Yang <suiyang03@gmail.com>
2021-08-23 13:12:49 +0800

    Time: 80 ms (97.95%), Space: 14.8 MB (66.28%) - LeetHub

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -3,9 +3,10 @@ class Solution:
         need, window = defaultdict(int), defaultdict(int)
         for c in t:
             need[c] += 1
-        left, right, valid = 0, 0, 0
-        start, start_len = 0, float('inf')
         
+        left, right, valid  = 0, 0, 0
+        start, start_len = 0, float('inf')
+
         while right < len(s):
             c = s[right]
             right += 1
@@ -19,11 +20,13 @@ class Solution:
                 if right - left < start_len:
                     start = left
                     start_len = right - left
+                    
                 d = s[left]
                 left += 1
                 if d in need:
                     if window[d] == need[d]:
                         valid -= 1
                     window[d] -= 1
-        if start_len == float('inf'): return ""
+
+        if start_len == float('inf'): return ''
         return s[start:start+start_len]

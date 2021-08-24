7e7fd71fffbc1cbf566a4d1540543a20d7540e6c
Sui Yang <suiyang03@gmail.com>
2021-08-24 13:13:00 +0800

    Time: 28 ms (84.69%), Space: 14.4 MB (71.70%) - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.py
+++ reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -4,10 +4,10 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         original_head = dummy = ListNode()
         window_head, window_tail, dummy.next = head, head, head
-        for _ in range(right-left):
+        for _ in range(right - left):
             window_tail = window_tail.next
         
         for _ in range(left-1):
@@ -15,15 +15,12 @@ class Solution:
             window_head = window_head.next
             window_tail = window_tail.next
         
-        original_head.next = None
         original_tail = window_tail.next
-        window_tail.next = None
-        
+        original_head.next, window_tail.next = None, None
         new_head, new_tail = self.reverse(window_head)
-        original_head.next = new_head
-        new_tail.next = original_tail
-        return dummy.next
+        original_head.next, new_tail.next = new_head, original_tail
         
+        return dummy.next
         
         
     def reverse(self, head):
@@ -33,48 +30,3 @@ class Solution:
         return prev, tail
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         original_head = dummy = ListNode(0)
-#         window_head, window_tail = head, head
-#         dummy.next = head
-#         for _ in range(right-left):
-#             window_tail = window_tail.next
-        
-#         for _ in range(left-1):
-#             original_head, window_head, window_tail = window_head, window_head.next, window_tail.next
-        
-#         original_tail, window_tail.next = window_tail.next, None
-#         reversed_head, reversed_tail = self.reverse(window_head)
-        
-#         original_head.next, reversed_tail.next = reversed_head, original_tail
-#         return dummy.next
-    
-#     def reverse(self, head):
-#         curr, tail, prev = head, head, None
-#         while curr:
-#             curr.next, prev, curr = prev, curr, curr.next
-#         return prev, tail

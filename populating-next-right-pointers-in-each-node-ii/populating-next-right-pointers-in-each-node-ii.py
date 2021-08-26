ec8fe46af8de8672b7a876fb8156a653f8cbfc57
Sui Yang <suiyang03@gmail.com>
2021-08-26 12:58:38 +0800

    Time: 75 ms (7.13%), Space: 15.5 MB (49.14%) - LeetHub

--- populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -10,60 +10,20 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        curr = root
-        p = dummy = Node()
-        while curr:
-            if curr.left:
-                p.next = curr.left
-                p = p.next
-            if curr.right:
-                p.next = curr.right
-                p = p.next
-            if curr.next:
-                curr = curr.next
-            else:
-                curr = dummy.next
-                dummy.next = None
-                p = dummy
-        return root
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         self.d = defaultdict(list)
-#         def dfs(node, level=0):
-#             if not node: return
-#             self.d[level].append(node)
-#             dfs(node.left, level+1)
-#             dfs(node.right, level+1)
-#         dfs(root)
-#         for l in self.d.values():
-#             for i in range(len(l)):
-#                 if i == len(l) - 1: l[i].next = None
-#                 else:
-#                     l[i].next = l[i+1]
-#         return root
-        
-        if not root: return
-        curr = root
-        p = dummy = Node()
-        while curr:
-            if curr.left:
-                p.next = curr.left
-                p = p.next
-            if curr.right:
-                p.next = curr.right
-                p = p.next
-            if curr.next:
-                curr = curr.next
-            else:
-                curr = dummy.next
-                dummy.next = None
-                p = dummy
+        if not root: return root
+        queue = [root]
+        while queue:
+            new_queue = []
+            queue.append(None)
+            for i in range(len(queue)-1):
+                queue[i].next = queue[i+1]
+            queue.pop()
+            
+            for node in queue:
+                if node:
+                    if node.left:
+                        new_queue.append(node.left)
+                    if node.right:
+                        new_queue.append(node.right)
+            queue = new_queue
         return root

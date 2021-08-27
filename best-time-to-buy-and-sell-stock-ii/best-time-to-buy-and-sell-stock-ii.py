7e5f7f1ded7610f8550786fa3bd6c86b09b25fdb
Sui Yang <suiyang03@gmail.com>
2021-08-27 12:57:19 +0800

    Time: 130 ms (5.35%), Space: 15.1 MB (23.05%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,41 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices: return 0
+        dp = [0] * len(prices)
         dp_i_0, dp_i_1 = 0, float('-inf')
-        for i in range(len(prices)):
-            temp = dp_i_0
-            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-            dp_i_1 = max(dp_i_1, temp - prices[i])
+        for p in prices:
+            dp_i_0 = max(dp_i_0, dp_i_1 + p)
+            dp_i_1 = max(dp_i_1, dp_i_0 - p)
         return dp_i_0
-        
-        
-        
-        
-#         i, n = 0, len(prices) - 1 
-#         buy, sell, profit = 0, 0, 0
-#         while i < n:
-#             while i < n and prices[i+1] <= prices[i]: i += 1
-#             buy = prices[i]
-#             while i < n and prices[i+1] >= prices[i]: i += 1
-#             sell = prices[i]
-            
-#             profit += sell - buy
-#         return profit
-
-        # dp_i_0, dp_i_1 = 0, float('-inf')
-        # for i in range(len(prices)):
-        #     temp = dp_i_0
-        #     dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-        #     dp_i_1 = max(dp_i_1, temp - prices[i])
-        # return dp_i_0
-        
-#         i, n = 0, len(prices) - 1
-#         buy, sell, profit = 0, 0, 0
-#         while i < n:
-#             while i < n and prices[i+1] <= prices[i]: i += 1
-#             buy = prices[i]
-#             while i < n and prices[i+1] >= prices[i]: i += 1
-#             sell = prices[i]
-            
-#             profit += sell - buy
-#         return profit

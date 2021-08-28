360f4b6b071b06334caf2bb1b8d666262339bffa
Sui Yang <suiyang03@gmail.com>
2021-08-28 16:19:47 +0800

    Time: 28 ms (83.08%), Space: 14.2 MB (75.54%) - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         self.res = []
         def dfs(node):
             if not node: return
@@ -14,64 +14,3 @@ class Solution:
             dfs(node.right)
         dfs(root)
         return self.res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # Time: O(N)
-        # Space: O(height)
-        self.res = []
-        def dfs(node):
-            if not node: return
-            self.res.append(node.val)
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return self.res
-        
-        # # Recursive 1
-        # return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
-        
-        # Recursive 2
-        # res = []
-        # def dfs(node):
-        #     if node:
-        #         res.append(node.val)
-        #         dfs(node.left)
-        #         dfs(node.right)
-        # dfs(root)
-        # return res
-    
-        # # Iterative
-        # if not root: return []
-        # res, stack = [], [root]
-        # while stack:
-        #     curr = stack.pop()
-        #     res.append(curr.val)
-        #     if curr.right: stack.append(curr.right)
-        #     if curr.left: stack.append(curr.left)
-            
-        # return res
-        
-        
-        
-        def preorder(node):
-            if node is None: return None
-            print(node.val)
-            inorder(node.left)
-            inorder(node.right)
-
-        preorder(root)
-        
-        
-        

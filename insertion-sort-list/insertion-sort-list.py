bf3cad8be22a7cdf85ea2665646a4bc84f935a32
Sui Yang <suiyang03@gmail.com>
2021-08-29 13:54:42 +0800

    Time: 192 ms (77.53%), Space: 16.4 MB (29.05%) - LeetHub

--- insertion-sort-list/insertion-sort-list.py
+++ insertion-sort-list/insertion-sort-list.py
@@ -4,16 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def insertionSortList(self, head: ListNode) -> ListNode:
-        # curr = dummy = ListNode(0)
-        # while head:
-        #     if curr.val > head.val:
-        #         curr = dummy
-        #     while curr.next and curr.next.val < head.val:
-        #         curr = curr.next
-        #     curr.next, curr.next.next, head = head, curr.next, head.next
-        # return dummy.next
-        
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         dummy.next = insert_node = head
         while head and head.next:

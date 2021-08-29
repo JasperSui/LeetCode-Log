c956f9f0e61bf91e1223049f1b57817076699266
Sui Yang <suiyang03@gmail.com>
2021-08-29 14:03:26 +0800

    Time: 476 ms (52.83%), Space: 30.1 MB (73.39%) - LeetHub

--- sort-list/sort-list.py
+++ sort-list/sort-list.py
@@ -4,10 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Merge Sort
-    # Time: O(nlogn)
-    # Space: O(nlogn)
-    def sortList(self, head: ListNode) -> ListNode:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next: return head
         mid = self.get_mid(head)
         left = self.sortList(head)
@@ -27,36 +24,9 @@ class Solution:
         dummy = tail = ListNode()
         while left and right:
             if left.val < right.val:
-                tail.next, tail, left = left, left, left.next
+                tail.next, left = left, left.next
             else:
-                tail.next, tail, right = right, right, right.next
+                tail.next, right = right, right.next
+            tail = tail.next
         tail.next = left or right
         return dummy.next
-        
-        
-        
-        
-#         if not head or not head.next: return head
-#         mid = self.get_mid(head)
-#         left = self.sortList(head)
-#         right = self.sortList(mid)
-#         return self.merge(left, right)
-        
-#     def get_mid(self, head):
-#         fast = slow = head
-#         while fast.next and fast.next.next:
-#             fast = fast.next.next
-#             slow = slow.next
-#         mid = slow.next
-#         slow.next = None
-#         return mid
-
-#     def merge(self, left, right):
-#         dummy = tail = ListNode(None)
-#         while left and right:
-#             if left.val < right.val:
-#                 tail.next, tail, left = left, left, left.next
-#             else:
-#                 tail.next, tail, right = right, right, right.next
-#         tail.next = left or right
-#         return dummy.next

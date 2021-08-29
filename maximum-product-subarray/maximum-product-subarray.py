72333bb9febe69b10138a42e5195e4cbedd97ffb
Sui Yang <suiyang03@gmail.com>
2021-08-29 14:47:03 +0800

    Time: 52 ms (83.95%), Space: 14.3 MB (63.20%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = _max = _min = nums[0]
-        
         for i in range(1, len(nums)):
             if nums[i] < 0:
                 _max, _min = _min, _max
@@ -11,3 +10,4 @@ class Solution:
             
             res = max(res, _max)
         return res
+            

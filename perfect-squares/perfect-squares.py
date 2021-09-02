c494d7c26db55956cfce1127f20ece0409bfd3c0
Sui Yang <suiyang03@gmail.com>
2021-09-02 13:03:05 +0800

    Time: 334 ms (77.17%), Space: 14.8 MB (37.23%) - LeetHub

--- perfect-squares/perfect-squares.py
+++ perfect-squares/perfect-squares.py
@@ -1,86 +1,22 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         if n <= 2: return n
-        squares = []
+        choices = []
         i = 1
-        while i*i <= n:
-            squares.append(i*i)
+        while i * i <= n:
+            choices.append(i*i)
             i += 1
         
         ans = 0
         stack = {n}
         while stack:
-            temp = set()
+            new_stack = set()
             ans += 1
             for i in stack:
-                for square in squares:
+                for square in choices:
                     if square == i:
                         return ans
                     if i < square:
                         break
-                    temp.add(i-square)
-            stack = temp
-        return ans
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        if n < 2: return n
-        squares = []
-        i = 1
-        while i*i <= n:
-            squares.append(i*i)
-            i += 1
-        
-        ans = 0
-        stack = {n}
-        while stack:
-            temp = set()
-            ans += 1
-            for i in stack:
-                for square in squares:
-                    if i == square:
-                        return ans
-                    if i < square:
-                        break
-                    temp.add(i-square)
-            stack = temp
-        return ans
+                    new_stack.add(i-square)
+            stack = new_stack

d905be3ccf28860d312438228edee6e111a30397
Sui Yang <suiyang03@gmail.com>
2021-09-03 13:10:29 +0800

    Time: 580 ms (98.74%), Space: 14.4 MB (80.92%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -5,18 +5,16 @@ class Solution:
         visited = [False] * (amount + 1)
         visited[0] = True
         ans = 0
-        
         while stack:
             new_stack = set()
             ans += 1
             for i in stack:
                 for j in coins:
                     new_amount = i + j
-                    if i + j == amount: return ans
-                    elif i + j > amount: continue
+                    if new_amount == amount: return ans
+                    elif new_amount > amount: continue
                     elif not visited[new_amount]:
                         visited[new_amount] = True
                         new_stack.add(new_amount)
             stack = new_stack
         return -1
-        

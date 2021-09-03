6334145a35c35aaa07ca41ecfe057ed5948c3970
Sui Yang <suiyang03@gmail.com>
2021-09-03 13:18:51 +0800

    Time: 670 ms (20.62%), Space: 16.6 MB (45.44%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         if not matrix: return 0
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         d = {}
         m, n = len(matrix), len(matrix[0])
         for i in range(m):
@@ -10,24 +10,22 @@ class Solution:
                 for dx, dy in directions:
                     x = i + dx
                     y = j + dy
-                    if 0 <= x <= m-1 and 0 <= y <= n-1 and matrix[x][y] < matrix[i][j]:
+                    if 0 <= x < m and 0 <= y < n and matrix[x][y] < matrix[i][j]:
                         count += 1
                 d[(i, j)] = count
         
-        queue = [k for k, v in d.items() if v == 0]
-        steps = 0    
-        
+        queue = [k for k, v in d.items() if not v]
+        step = 0
         while queue:
             new_queue = []
-            steps += 1
+            step += 1
             for i, j in queue:
                 for dx, dy in directions:
                     x = i + dx
                     y = j + dy
-                    if 0 <= x <= m-1 and 0 <= y <= n-1 and matrix[x][y] > matrix[i][j] and (x, y) in d:
+                    if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[i][j] and (x, y) in d:
                         d[(x, y)] -= 1
                         if d[(x, y)] == 0:
                             new_queue.append((x, y))
             queue = new_queue
-        return steps
-                    
+        return step

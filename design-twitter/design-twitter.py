7add39036545944478fc7f22dc6c8487e5b8a0ba
Sui Yang <suiyang03@gmail.com>
2021-09-04 14:26:38 +0800

    Time: 40 ms (16.78%), Space: 14.6 MB (26.02%) - LeetHub

--- design-twitter/design-twitter.py
+++ design-twitter/design-twitter.py
@@ -4,45 +4,44 @@ class Twitter:
         """
         Initialize your data structure here.
         """
-        self.user_d = defaultdict(set)
-        self.tweet_d = defaultdict(list)
-        self.tweet_id = 1
+        self.id = 1
+        self.tweets = defaultdict(list)
+        self.follow_dict = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         """
         Compose a new tweet.
         """
-        self.tweet_d[userId].append((tweetId, self.tweet_id))
-        self.tweet_id += 1
+        self.tweets[userId].append((tweetId, self.id))
+        self.id += 1
         
 
     def getNewsFeed(self, userId: int) -> List[int]:
         """
         Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
         """
-        user_list = self.user_d[userId]
-        tweet_list = self.tweet_d[userId][:]
-        for user_id in user_list:
-            tweet_list.extend(self.tweet_d[user_id])
-        tweet_list = sorted(tweet_list, key=lambda x:x[1], reverse=True)
-        res = [tweet_id for tweet_id, _ in tweet_list]
-        return res[:10] if len(res) >= 10 else res
+        subs = [userId, *list(self.follow_dict[userId])]
+        tweets = []
+        for user_id in subs:
+            tweets.extend(self.tweets[user_id][-10:])
+        tweets.sort(key=lambda x: -x[1])
+        return [tweet_id for tweet_id, _ in tweets[:10]]
+        
         
 
     def follow(self, followerId: int, followeeId: int) -> None:
         """
         Follower follows a followee. If the operation is invalid, it should be a no-op.
         """
-        self.user_d[followerId].add(followeeId)
+        self.follow_dict[followerId].add(followeeId)
         
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         """
         Follower unfollows a followee. If the operation is invalid, it should be a no-op.
         """
-        if followerId in self.user_d:
-            if followeeId in self.user_d[followerId]:
-                self.user_d[followerId].remove(followeeId)
+        if followerId in self.follow_dict:
+            self.follow_dict[followerId].discard(followeeId)
         
 
 

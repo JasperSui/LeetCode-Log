9d3e3fa49861d9edb11424eda2b7a96ead328294
Sui Yang <suiyang03@gmail.com>
2021-09-04 15:08:44 +0800

    Time: 61 ms (11.62%), Space: 14.4 MB (25.19%) - LeetHub

--- combination-sum-iv/combination-sum-iv.py
+++ combination-sum-iv/combination-sum-iv.py
@@ -5,8 +5,7 @@ class Solution:
         return self.helper(nums, target, dp)
     
     def helper(self, nums, target, dp):
-        if dp[target] != -1:
-            return dp[target]
+        if dp[target] != -1: return dp[target]
         
         res = 0
         for i in range(len(nums)):
@@ -14,20 +13,3 @@ class Solution:
                 res += self.helper(nums, target-nums[i], dp)
         dp[target] = res
         return res
-        
-#         res = set()
-#         visited = set()
-#         self.dfs(nums, '', target, res, visited)
-#         return len(res)
-    
-#     def dfs(self, nums, path, target, res, visited):
-#         if path in visited:
-#             return
-#         visited.add(path)
-#         if target < 0:
-#             return
-#         elif target == 0:
-#             res.add(path)
-#             return
-#         for n in nums:
-#             self.dfs(nums, path + str(n), target - n, res, visited)

e70d7ee91006e54345b9f9eede7b53584e7ef427
Sui Yang <suiyang03@gmail.com>
2021-09-05 13:44:19 +0800

    Time: 36 ms (89.75%), Space: 14.3 MB (93.95%) - LeetHub

--- remove-k-digits/remove-k-digits.py
+++ remove-k-digits/remove-k-digits.py
@@ -6,7 +6,7 @@ class Solution:
                 stack.pop()
                 k -= 1
             stack.append(n)
+        
         if k > 0:
             stack = stack[:-k]
         return "".join(stack).lstrip("0") or "0"
-            

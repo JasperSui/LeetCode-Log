18258551edb050f04c53ea499ca61e0ec333cc46
Sui Yang <suiyang03@gmail.com>
2021-09-05 13:39:13 +0800

    Time: 32 ms (66.10%), Space: 14.2 MB (75.84%) - LeetHub

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -1,27 +1,26 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        d = defaultdict(set)
+        graph = defaultdict(set)
         for (x, y), v in zip(equations, values):
-            d[x].add((y, v))
-            d[y].add((x, 1/v))
+            graph[x].add((y, v))
+            graph[y].add((x, 1/v))
         
         def find(a, b):
-            if a not in d or b not in d:
+            if a not in graph or b not in graph:
                 return -1.0
+            
             queue = [(a, 1.0)]
             visited = set()
-            
             while queue:
                 new_queue = []
-                for front, curr_val in queue:
-                    if front == b:
-                        return curr_val
-                    visited.add(front)
-                    for neigh, value in d[front]:
+                for char, last_value in queue:
+                    visited.add(char)
+                    if char == b:
+                        return last_value
+                    
+                    for neigh, value in graph[char]:
                         if neigh not in visited:
-                            new_queue.append((neigh, value * curr_val))
+                            new_queue.append((neigh, last_value * value))
                 queue = new_queue
-            return -1.0
-        
+            return -1
         return [find(x, y) for x, y in queries]
-            

b3c7f54588bb7a6912819b0c01d9d52e33ef353e
Sui Yang <suiyang03@gmail.com>
2021-09-05 13:48:29 +0800

    Time: 92 ms (89.54%), Space: 14.8 MB (60.54%) - LeetHub

--- queue-reconstruction-by-height/queue-reconstruction-by-height.py
+++ queue-reconstruction-by-height/queue-reconstruction-by-height.py
@@ -1,5 +1,6 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        # 按照身高（倒序）和index（順序）排，之後再直接 insert，因為那批高的只會算那批的位置所以不管其他人
         res = []
         for p in sorted(people, key=lambda x: (-x[0], x[1])):
             res.insert(p[1], p)

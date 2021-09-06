e646c97d961b86bdb05f8ef5dadadc1faec65665
Sui Yang <suiyang03@gmail.com>
2021-09-06 13:08:15 +0800

    Time: 288 ms (68.40%), Space: 15.5 MB (52.84%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,18 +1,18 @@
 class Solution:
     def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        res = []
         m, n = len(matrix), len(matrix[0])
         p_visited = [[False for _ in range(n)] for _ in range(m)]
         a_visited = [[False for _ in range(n)] for _ in range(m)]
         
         for i in range(m):
-            self.dfs(matrix, i, 0, m, n, p_visited)
-            self.dfs(matrix, i, n-1, m, n, a_visited)
+            self.dfs(matrix, i, 0, m, n, p_visited, res)
+            self.dfs(matrix, i, n-1, m, n, a_visited, res)
         
         for j in range(n):
-            self.dfs(matrix, 0, j, m, n, p_visited)
-            self.dfs(matrix, m-1, j, m, n, a_visited)
+            self.dfs(matrix, 0, j, m, n, p_visited, res)
+            self.dfs(matrix, m-1, j, m, n, a_visited, res)
         
-        res = []
         for i in range(m):
             for j in range(n):
                 if p_visited[i][j] and a_visited[i][j]:
@@ -20,10 +20,12 @@ class Solution:
         
         return res
     
-    def dfs(self, matrix, i, j, m, n, visited):
-        visited[i][j] = 1
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        for dir in directions:
-            x, y = i + dir[0], j + dir[1]
-            if x < 0 or y < 0 or x >= m or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]: continue
-            self.dfs(matrix, x, y, m, n, visited)
+    def dfs(self, matrix, i, j, m, n, visited, res):
+        visited[i][j] = True
+        directions = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+        for dx, dy in directions:
+            x = i + dx
+            y = j + dy
+            if x < 0 or y < 0 or x >= m or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]:
+                continue
+            self.dfs(matrix, x, y, m, n, visited, res)

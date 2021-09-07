89432ad106b06919c802e546e1e27bf622124908
Sui Yang <suiyang03@gmail.com>
2021-09-07 12:57:38 +0800

    Time: 1176 ms (25.91%), Space: 59 MB (21.56%) - LeetHub

--- minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -4,7 +4,7 @@ class Solution:
         prev = float('-inf')
         points.sort(key=lambda x: x[1])
         for s, e in points:
-            if prev >= s: continue
+            if s <= prev: continue    
             prev = e
             ans += 1
         return ans

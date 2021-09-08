f5a4948e550d24472aabb2b95eb2463cff8eed1a
Sui Yang <suiyang03@gmail.com>
2021-09-08 13:03:16 +0800

    Time: 48 ms (36.62%), Space: 17.3 MB (7.48%) - LeetHub

--- find-bottom-left-tree-value/find-bottom-left-tree-value.py
+++ find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -5,14 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findBottomLeftValue(self, root: TreeNode) -> int:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         self.d = defaultdict(list)
-        self.max_depth = float('-inf')
         def dfs(node, depth=1):
             if not node: return
             self.d[depth].append(node.val)
-            self.max_depth = max(self.max_depth, depth)
             dfs(node.left, depth+1)
             dfs(node.right, depth+1)
         dfs(root)
-        return self.d[self.max_depth][0]
+        return self.d[max(self.d.keys())][0]

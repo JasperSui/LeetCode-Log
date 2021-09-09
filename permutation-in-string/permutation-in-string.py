3233ef682d8c80ac3c598dffce4c4a633494ec03
Sui Yang <suiyang03@gmail.com>
2021-09-09 13:08:15 +0800

    Time: 72 ms (70.23%), Space: 14.2 MB (96.40%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -7,14 +7,14 @@ class Solution:
         while right < len(s2):
             c = s2[right]
             right += 1
-            
             if c in need:
                 window[c] += 1
                 if window[c] == need[c]:
                     valid += 1
             
-            while right - left >= len(s1):
-                if valid == len(need): return True
+            while right - left == len(s1):
+                if valid == len(need):
+                    return True
                 d = s2[left]
                 left += 1
                 if d in need:
@@ -22,4 +22,3 @@ class Solution:
                         valid -= 1
                     window[d] -= 1
         return False
-                

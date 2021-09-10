6e16ef7c92c112341d4c0453dbffbc8a1c9f54df
Sui Yang <suiyang03@gmail.com>
2021-09-10 12:47:58 +0800

    Time: 593 ms (27.27%), Space: 14.7 MB (74.01%) - LeetHub

--- task-scheduler/task-scheduler.py
+++ task-scheduler/task-scheduler.py
@@ -1,20 +1,18 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         d = defaultdict(int)
-        ma = 0
-        max_count = 0
+        max_count, max_tasks = 0, 0
         for c in tasks:
-            index = ord(c) - ord('A')
-            d[index] += 1
-            if d[index] > ma:
-                ma = d[index]
-                max_count = 1
-            elif d[index] == ma:
-                max_count += 1
+            d[c] += 1
+            if d[c] > max_count:
+                max_count = d[c]
+                max_tasks = 1
+            elif d[c] == max_count:
+                max_tasks += 1
         
-        part_count = ma - 1
-        part_len = n - (max_count - 1)
+        part_count = max_count - 1
+        part_len = n - (max_tasks - 1)
         empty_slots = part_count * part_len
-        available_tasks = len(tasks) - (ma * max_count)
+        available_tasks = len(tasks) - max_count * max_tasks
         idles = max(0, empty_slots - available_tasks)
         return len(tasks) + idles

12f69b4f04a053d9ed8f27717edc49b619d3c415
Sui Yang <suiyang03@gmail.com>
2021-09-12 11:42:08 +0800

    Time: 47 ms (64.73%), Space: 14.1 MB (92.45%) - LeetHub

--- 2-keys-keyboard/2-keys-keyboard.py
+++ 2-keys-keyboard/2-keys-keyboard.py
@@ -4,15 +4,5 @@ class Solution:
         for i in range(2, n+1):
             while n % i == 0:
                 res += i
-                n = n / i
+                n /= i
         return res
-        
-#         dp = [0] * (n+1)
-        
-#         for i in range(2, n+1):
-#             dp[i] = i
-#             for j in range(i-1, 1, -1):
-#                 if i % j == 0:
-#                     dp[i] = dp[j] + (i/j)
-#                     break
-#         return int(dp[n])

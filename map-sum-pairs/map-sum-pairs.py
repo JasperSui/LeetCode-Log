7778049aa6c632a73b061232d013586f5202790b
Sui Yang <suiyang03@gmail.com>
2021-09-12 15:40:42 +0800

    Time: 51 ms (20.08%), Space: 14.5 MB (24.35%) - LeetHub

--- map-sum-pairs/map-sum-pairs.py
+++ map-sum-pairs/map-sum-pairs.py
@@ -1,42 +1,52 @@
 class TrieNode:
     def __init__(self):
-        self.val = 0
-        self.is_word = False
         self.links = defaultdict(TrieNode)
+        self.points = 0
 
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word, points):
+        node = self.root
+        for c in word:
+            node = node.links[c]
+        node.points = points
+    
+    def get_points(self, node):
+        points = node.points
+        for child in node.links.values():
+            points += self.get_points(child)
+        return points
+    
+    def find(self, word):
+        depth = 0
+        node = self.root
+        if not word:
+            return self.get_points
+        for c in word:
+            if c not in node.links: return 0
+            depth += 1
+            node = node.links[c]
+            if depth == len(word):
+                return self.get_points(node)
+        return 0
+        
+        
 class MapSum:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.root = TrieNode()
-        
+        self.t = Trie()
 
     def insert(self, key: str, val: int) -> None:
-        node = self.root
-        for c in key:
-            node = node.links[c]
-        node.is_word = True
-        node.val = val
-
+        self.t.insert(key, val)
         
     def sum(self, prefix: str) -> int:
-        node = self.root
-        for c in prefix:
-            next_node = node.links[c]
-            if not next_node:
-                return 0
-            node = next_node
-        return self.dfs(node)
-    
-    def dfs(self, node):
-        ans = 0
-        for child in node.links.keys():
-            ans += self.dfs(node.links[child])
-        return ans + node.val
+        return self.t.find(prefix)
 
-    
 
 # Your MapSum object will be instantiated and called as such:
 # obj = MapSum()

0556abfd192ee0c97533cbce729d2dc95c50c776
Sui Yang <suiyang03@gmail.com>
2021-09-12 15:01:46 +0800

    Time: 796 ms (89.08%), Space: 14.3 MB (52.56%) - LeetHub

--- kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
+++ kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -1,19 +1,10 @@
 class Solution:
     def findKthNumber(self, m: int, n: int, k: int) -> int:
-        low, high = 1, m*n
+        low, high = 0, m*n
         while low < high:
-            mid = low + (high-low) // 2
-            if sum(min(mid//i, n) for i in range(1, m+1)) < k:
+            mid = low + (high - low) // 2
+            if sum(min(mid // i, n) for i in range(1, m+1)) < k:
                 low = mid + 1
             else:
                 high = mid
         return low
-        
-        
-        # low, high = 1, m*n
-        # while low < high:
-        #     mid = (low+high) // 2
-        #     if sum(min(mid//i, n) for i in range(1, m+1)) < k:
-        #         low = mid + 1
-        #     else: high = mid
-        # return low

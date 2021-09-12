2ba2ca83a5dbc5f0adf4ddf9b3bc11136c44cdac
Sui Yang <suiyang03@gmail.com>
2021-09-12 15:26:22 +0800

    Time: 426 ms (18.33%), Space: 18.3 MB (13.17%) - LeetHub

--- implement-magic-dictionary/implement-magic-dictionary.py
+++ implement-magic-dictionary/implement-magic-dictionary.py
@@ -1,15 +1,51 @@
-class MagicDictionary:
+class TrieNode:
+    def __init__(self):
+        self.links = defaultdict(TrieNode)
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
     
-    def _candidates(self, word):
-        for i in range(len(word)):
-            yield word[:i] + "*" + word[i+1:]
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            node = node.links[c]
+        node.is_word = True
+    
+    def search(self, word, remain, node=None):
+        if not node:
+            node = self.root
+        
+        if not word:
+            if not remain and node.is_word:
+                return True
+            return False
+        
+        for key in node.links.keys():
+            if key == word[0]:
+                if self.search(word[1:], remain, node.links[key]):
+                    return True
+            elif remain == 1:
+                if self.search(word[1:], 0, node.links[key]):
+                    return True
+        return False
+        
+class MagicDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.t = Trie()
 
     def buildDict(self, dictionary: List[str]) -> None:
-        self.words = set(dictionary)
-        self.near = collections.Counter(cand for word in self.words for cand in self._candidates(word))
+        for word in dictionary:
+            self.t.insert(word)
 
     def search(self, searchWord: str) -> bool:
-        return any(self.near[cand] > 1 or self.near[cand] == 1 and searchWord not in self.words for cand in self._candidates(searchWord))
+        return self.t.search(searchWord, 1)
+
 
 # Your MagicDictionary object will be instantiated and called as such:
 # obj = MagicDictionary()

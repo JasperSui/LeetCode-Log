54efcab672249b902961cd9ef80bb7d8ae8923ee
Sui Yang <suiyang03@gmail.com>
2021-09-14 12:51:29 +0800

    Time: 72 ms (87.09%), Space: 14.5 MB (52.51%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,15 +1,17 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        m, n = len(image), len(image[0])
-        self.dfs(image, sr, sc, m, n, image[sr][sc], newColor)
-        return image
-    
-    def dfs(self, image, i, j, m, n, original_color, new_color):
-        if i < 0 or j < 0 or i >= m or j >= n or image[i][j] != original_color or image[i][j] == new_color: return
-
-        image[i][j] = new_color
+    def floodFill(self, grid: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        color = grid[sr][sc]
         
-        self.dfs(image, i, j+1, m, n, original_color, new_color)
-        self.dfs(image, i, j-1, m, n, original_color, new_color)
-        self.dfs(image, i+1, j, m, n, original_color, new_color)
-        self.dfs(image, i-1, j, m, n, original_color, new_color)
+        m, n = len(grid), len(grid[0])
+        self.dfs(grid, sr, sc, m, n, color, newColor)
+        return grid
+        
+    def dfs(self, grid, i, j, m, n, color, new_color):
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != color or grid[i][j] == new_color: return
+        
+        grid[i][j] = new_color
+        self.dfs(grid, i+1, j, m, n, color, new_color)
+        self.dfs(grid, i-1, j, m, n, color, new_color)
+        self.dfs(grid, i, j+1, m, n, color, new_color)
+        self.dfs(grid, i, j-1, m, n, color, new_color)
+        

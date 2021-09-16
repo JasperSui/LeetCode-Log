972d209ec2199696ef119920c68a6c2b8c5a0c36
Sui Yang <suiyang03@gmail.com>
2021-09-16 12:52:11 +0800

    Time: 78 ms (5.10%), Space: 14.2 MB (73.82%) - LeetHub

--- most-common-word/most-common-word.py
+++ most-common-word/most-common-word.py
@@ -1,44 +1,29 @@
 class TrieNode:
     def __init__(self):
-        self.count = 0
-        self.word = ""
         self.links = defaultdict(TrieNode)
+        self.count = 0
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
         self.max_count = 0
-        self.max_word = ""
+        self.res = ""
     
     def insert(self, word):
         node = self.root
         for c in word:
             node = node.links[c]
         node.count += 1
-        node.word = word
-    
-    def ban(self, word):
-        node = self.root
-        for c in word:
-            node = node.links[c]
-        node.count = 0
-    
-    def find_max(self, node=None):
-        if not node: 
-            node = self.root
         if node.count > self.max_count:
             self.max_count = node.count
-            self.max_word = node.word
-        for key in node.links.keys():
-            self.find_max(node.links[key])
-        return self.max_word
+            self.res = word
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = re.findall('\w+', paragraph.lower())
+        banned = set(banned)
         t = Trie()
-        words = re.findall(r'\w+', paragraph.lower())
         for word in words:
+            if word in banned: continue
             t.insert(word)
-        for word in banned:
-            t.ban(word)
-        return t.find_max()
+        return t.res

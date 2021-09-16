50f9fe2e8d672bea015c53e732cb65b8cb1b0083
Sui Yang <suiyang03@gmail.com>
2021-09-16 13:01:38 +0800

    Time: 408 ms (40.47%), Space: 16.1 MB (80.40%) - LeetHub

--- push-dominoes/push-dominoes.py
+++ push-dominoes/push-dominoes.py
@@ -3,9 +3,11 @@ class Solution:
         d = "L" + dominoes + "R"
         res = ""
         i = 0
+        
         for j in range(1, len(d)):
             if d[j] == ".":
                 continue
+            
             if i:
                 res += d[i]
             
@@ -13,12 +15,11 @@ class Solution:
             if d[i] == d[j]:
                 res += d[i] * space
             
-            elif d[j] == "R" and d[i] == "L":
+            elif d[i] == "L" and d[j] == "R":
                 res += "." * space
             
             else:
                 res += "R" * (space // 2) + "." * (space % 2) + "L" * (space // 2)
+            
             i = j
         return res
-                
-            

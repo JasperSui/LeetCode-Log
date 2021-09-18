6f63138d0906b52b4e82d95e1c48f4e2f9960044
Sui Yang <suiyang03@gmail.com>
2021-09-18 23:35:29 +0800

    Time: 616 ms (26.92%), Space: 15.6 MB (19.44%) - LeetHub

--- koko-eating-bananas/koko-eating-bananas.py
+++ koko-eating-bananas/koko-eating-bananas.py
@@ -3,70 +3,8 @@ class Solution:
         low, high = 1, max(piles)
         while low < high:
             mid = low + (high - low) // 2
-            if sum((p+mid - 1)//mid for p in piles) > h:
+            if sum(math.ceil(p / mid) for p in piles) > h:
                 low = mid + 1
             else:
                 high = mid
         return low
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # Binary Search
-        # Time: O(nlog(maxP))
-        # Space: O(1)
-        low, high = 1, max(piles)
-        while low < high:
-            mid = (low+high) // 2
-            if sum((p+mid)//mid for p in piles) > h:
-                low = mid + 1
-            else:
-                high = mid
-        return low
-        
-        # Shitty try
-        # lp = len(piles)
-        # piles.sort()
-        # if lp <= h < lp*2: return piles[lp-h-1]
-        # else:
-        #     ans = piles[0]
-        #     temp_h = 0
-        #     while temp_h != h:
-        #         for p in piles:
-                    

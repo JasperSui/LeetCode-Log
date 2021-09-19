03fc49b86c8a3e78634aeb602c413b8508c0607d
Sui Yang <suiyang03@gmail.com>
2021-09-20 00:48:39 +0800

    Time: 496 ms (91.26%), Space: 15.6 MB (93.48%) - LeetHub

--- number-of-enclaves/number-of-enclaves.py
+++ number-of-enclaves/number-of-enclaves.py
@@ -2,19 +2,20 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         for i in range(m):
-            if grid[i][0]:
-                self.dfs(grid, i, 0, m, n)
-            if grid[i][n-1]:
-                self.dfs(grid, i, n-1, m, n)
+            self.dfs(grid, i, 0, m, n)
+            self.dfs(grid, i, n-1, m, n)
         
-        for j in range(n):
-            if grid[0][j]:
-                self.dfs(grid, 0, j, m, n)
-            if grid[m-1][j]:
-                self.dfs(grid, m-1, j, m, n)
+        for i in range(n):
+            self.dfs(grid, 0, i, m, n)
+            self.dfs(grid, m-1, i, m, n)
         
-        return sum(grid[i][j] for i in range(m) for j in range(n))
-        
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    ans += 1
+        return ans
+    
     def dfs(self, grid, i, j, m, n):
         if i < 0 or j < 0 or i >= m or j >= n or not grid[i][j]: return
         grid[i][j] = 0

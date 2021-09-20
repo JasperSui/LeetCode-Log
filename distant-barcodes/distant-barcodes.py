623eb16326309c30dfdcb7ad714c7434dbd8d3b4
Sui Yang <suiyang03@gmail.com>
2021-09-20 16:32:40 +0800

    Time: 496 ms (56.00%), Space: 16.8 MB (22.00%) - LeetHub

--- distant-barcodes/distant-barcodes.py
+++ distant-barcodes/distant-barcodes.py
@@ -1,15 +1,15 @@
 class Solution:
     def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
-        res = []
-        d = collections.Counter(barcodes)
-        pq = [(-times, num) for num, times in d.items()]
+        d = Counter(barcodes)
+        pq = [(-times, key) for key, times in d.items()]
         heapq.heapify(pq)
-        prev_a, prev_b = 0, 0
-        while len(barcodes) != len(res):
+        prev_a, prev_b = 0, ""
+        res = []
+        for _ in range(len(barcodes)):
             a, b = heapq.heappop(pq)
-            res.append(b)
             if prev_a < 0:
                 heapq.heappush(pq, (prev_a, prev_b))
+            res.append(b)
             a += 1
             prev_a, prev_b = a, b
         return res

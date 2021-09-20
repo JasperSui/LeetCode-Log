b18b00a2235e63cdb0a229f93b940c0ef945686b
Sui Yang <suiyang03@gmail.com>
2021-09-20 17:01:58 +0800

    Time: 46 ms (16.88%), Space: 14.2 MB (40.26%) - LeetHub

--- split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -1,10 +1,12 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        r = l = ans = 0
+        l = r = ans = 0
         for c in s:
-            if c == 'R': r += 1
-            else: l += 1
-            if r == l:
+            if c == "R":
+                r += 1
+            else:
+                l += 1
+            if l == r:
+                l = r = 0
                 ans += 1
-                r = l = 0
         return ans

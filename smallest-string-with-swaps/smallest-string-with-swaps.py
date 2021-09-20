1020fc8e6a26cdb5e5fbd7b450e73c708398872d
Sui Yang <suiyang03@gmail.com>
2021-09-20 16:56:07 +0800

    Time: 1058 ms (28.67%), Space: 51 MB (42.38%) - LeetHub

--- smallest-string-with-swaps/smallest-string-with-swaps.py
+++ smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -3,32 +3,25 @@ class Solution:
         n = len(s)
         parent = list(range(n))
         memo = defaultdict(list)
-        res = []
-        
-        def union(x, y):
-            parent[find(y)] = find(x)
         
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
         
-        # make x == y's parent
+        def union(x, y):
+            parent[find(x)] = find(y)
+        
         for x, y in pairs:
             union(x, y)
-         
-        # find parent of i and append current char into the list
+        
         for i in range(n):
             memo[find(i)].append(s[i])
         
-        # Iterate all parent's list and sort them
-        for key in memo.keys():
-            memo[key].sort(reverse=True)
+        for k in memo:
+            memo[k].sort(reverse=True)
         
-        # Append the smallest char of i's list
+        res = []
         for i in range(n):
             res.append(memo[find(i)].pop())
-        
         return "".join(res)
-        
-            

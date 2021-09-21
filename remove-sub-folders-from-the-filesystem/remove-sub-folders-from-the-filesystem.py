a664dec18c21914ab0e2b3aecad465b22d821a7f
Sui Yang <suiyang03@gmail.com>
2021-09-21 16:55:08 +0800

    Time: 440 ms (29.29%), Space: 48.8 MB (10.80%) - LeetHub

--- remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -2,36 +2,34 @@ class TrieNode:
     def __init__(self):
         self.links = defaultdict(TrieNode)
         self.is_word = False
-        self.word = ""
 
 class Trie:
-    
     def __init__(self):
         self.root = TrieNode()
     
-    def insert(self, path):
+    def insert(self, word):
+        word = word.split('/')[1:]
         node = self.root
-        for c in path:
+        for c in word:
             node = node.links[c]
         node.is_word = True
-        node.word = "".join(["/"+c for c in path])
     
     def bfs(self):
         res = []
-        queue = deque([self.root])
+        queue = deque([(self.root, [])])
         while queue:
-            curr = queue.popleft()
-            if curr.is_word:
-                res.append(curr.word)
+            node, path = queue.popleft()
+            if node.is_word:
+                res.append("/" + "/".join(path))
                 continue
-            for node in curr.links.values():
-                queue.append(node)
+            
+            for c in node.links.keys():
+                queue.append((node.links[c], path + [c]))
         return res
 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         t = Trie()
-        for path in folder:
-            t.insert(path.split('/')[1:])
+        for word in folder:
+            t.insert(word)
         return t.bfs()
-            

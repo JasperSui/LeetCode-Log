4a7b9399860b929f61a0dc2eaa9dd9718273a732
Sui Yang <suiyang03@gmail.com>
2021-09-22 00:03:15 +0800

    Time: 132 ms (78.36%), Space: 14.7 MB (67.32%) - LeetHub

--- number-of-closed-islands/number-of-closed-islands.py
+++ number-of-closed-islands/number-of-closed-islands.py
@@ -1,32 +1,29 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        
         for i in range(m):
-            if not grid[i][0]:
-                self.dfs(grid, i, 0, m, n)
-            if not grid[i][n-1]:
-                self.dfs(grid, i, n-1, m, n)
-                
-        for j in range(n):
-            if not grid[0][j]:
-                self.dfs(grid, 0, j, m, n)
-            if not grid[m-1][j]:
-                self.dfs(grid, m-1, j, m, n)
-
+            self.dfs(grid, i, 0, m, n, 1)
+            self.dfs(grid, i, n-1, m, n, 1)
+        
+        for i in range(n):
+            self.dfs(grid, 0, i, m, n, 1)
+            self.dfs(grid, m-1, i, m, n, 1)
+        
         ans = 0
         for i in range(m):
             for j in range(n):
                 if not grid[i][j]:
-                    self.dfs(grid, i, j, m, n)
+                    self.dfs(grid, i, j, m, n, 1)
                     ans += 1
         return ans
-    
-    def dfs(self, grid, i, j, m, n):
-        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j]: return 0
         
-        grid[i][j] = 1
-        self.dfs(grid, i+1, j, m, n)
-        self.dfs(grid, i-1, j, m, n)
-        self.dfs(grid, i, j+1, m, n)
-        self.dfs(grid, i, j-1, m, n)
+        
+    def dfs(self, grid, i, j, m, n, target):
+        curr = abs(target - 1)
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != curr or grid[i][j] == target: return
+        grid[i][j] = target
+        
+        self.dfs(grid, i-1, j, m, n, target)
+        self.dfs(grid, i+1, j, m, n, target)
+        self.dfs(grid, i, j-1, m, n, target)
+        self.dfs(grid, i, j+1, m, n, target)

f37e4f2caf905661e06698f4e9a029d9cbf45304
Sui Yang <suiyang03@gmail.com>
2021-09-22 00:08:47 +0800

    Time: 488 ms (66.86%), Space: 15.8 MB (58.14%) - LeetHub

--- count-servers-that-communicate/count-servers-that-communicate.py
+++ count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,16 +1,15 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        row, col = [0] * m, [0] * n
+        row, col = [0] * n, [0] * m
         for i in range(m):
             for j in range(n):
                 if grid[i][j]:
-                    row[i] += 1
-                    col[j] += 1
-        
+                    col[i] += 1
+                    row[j] += 1
         ans = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] and (row[i] > 1 or col[j] > 1):
+                if grid[i][j] and (row[j] > 1 or col[i] > 1):
                     ans += 1
         return ans

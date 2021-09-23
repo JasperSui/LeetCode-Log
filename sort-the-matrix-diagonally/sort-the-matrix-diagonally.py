f64b69ecd0beca387928ad2bb258eb7532894a99
Sui Yang <suiyang03@gmail.com>
2021-09-23 12:55:25 +0800

    Time: 84 ms (76.79%), Space: 14.5 MB (77.66%) - LeetHub

--- sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
+++ sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
@@ -1,18 +1,16 @@
 class Solution:
-    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        m = len(mat[0])
-        n = len(mat)
+    def diagonalSort(self, grid: List[List[int]]) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
         d = defaultdict(list)
         
-        # 想像以 mat[0][0], mat[1][1] 為基準點， 每一條線的左右每個點距離都是一樣的
-        # 像是 mat[0][1] 距離 mat[0][0] 是 1, mat[1][2] 距離 mat[1][1] 也是 1
-        # 所以他們會落在同一個 d[距離差] 裡
-        for i in range(n):
-            for j in range(m):
-                d[i - j].append(mat[i][j])
+        for i in range(m):
+            for j in range(n):
+                d[i - j].append(grid[i][j])
+        
         for k in d:
             d[k].sort(reverse=True)
-        for i in range(n):
-            for j in range(m):
-                mat[i][j] = d[i-j].pop()
-        return mat
+        
+        for i in range(m):
+            for j in range(n):
+                grid[i][j] = d[i - j].pop()
+        return grid
